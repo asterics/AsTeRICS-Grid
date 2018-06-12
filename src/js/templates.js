871d4ff416f16bf692640d4b3c7c49bb3b90ecb5
Benjamin Klaus <klaus@technikum-wien.at>
2018-06-12 14:09:28 +0200

    fixed id-generation for grid items

--- src/js/templates.js
+++ src/js/templates.js
@@ -1,10 +1,11 @@
 var tempates = {};
+var lastId = 1;
 tempates.getGridItem = function (number, sizeX, sizeY) {
     sizeX = sizeX || 1;
     sizeY = sizeY || 1;
 
     var template = `
-<li id="${'grid-item-' + new Date().getTime()}" class="item" data-w="${sizeX}" data-h="${sizeY}">
+<li id="${'grid-item-' + lastId++}" class="item" data-w="${sizeX}" data-h="${sizeY}">
     <div class="grid-item-content">
         ${number}
     </div>
