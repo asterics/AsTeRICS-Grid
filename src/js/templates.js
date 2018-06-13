911131b8ae11c40aadf37ededdadde92094a0545
Benjamin Klaus <klaus@technikum-wien.at>
2018-06-13 16:18:38 +0200

    adapted to be filled with GridElement data, added gridBase template

--- src/js/templates.js
+++ src/js/templates.js
@@ -1,16 +1,29 @@
-var tempates = {};
+var templates = {};
 var lastId = 1;
-tempates.getGridItem = function (number, sizeX, sizeY) {
-    sizeX = sizeX || 1;
-    sizeY = sizeY || 1;
+
+templates.getGridBase = function (gridId) {
+    return `<ul id="${gridId}" class="grid">
+                <li class="position-highlight" style="display: none;">
+                    <div class="grid-item-content"></div>
+                </li>
+            </ul>`;
+};
+
+
+templates.getGridItem = function (label, width, height, posX, posY, id) {
+    width = width || 1;
+    height = height || 1;
+    posX = posX || 0;
+    posY = posY || 0;
+    id = id || lastId++;
 
     var template = `
-<li id="${'grid-item-' + lastId++}" class="item" data-w="${sizeX}" data-h="${sizeY}">
-    <div class="grid-item-content">
-        ${number}
+<li class="item" data-w="${width}" data-h="${height}" data-x="${posX}" data-y="${posY}">
+    <div class="grid-item-content" id="${id}">
+        ${label}
     </div>
 </li>`;
     return template;
 };
 
-export {tempates};
+export {templates};
