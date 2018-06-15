f8fd8f871b8b2ff3ec3dbe3c4ffcdf2fb9b168e6
Benjamin Klaus <klaus@technikum-wien.at>
2018-06-15 15:15:26 +0200

    added method to parse from JSON, added method hasSetPosition(), removed default x and y values

--- src/js/model/GridElement.js
+++ src/js/model/GridElement.js
@@ -5,8 +5,8 @@ class GridElement extends Model({
     id: String,
     width: Number,
     height: Number,
-    x: Number,
-    y: Number,
+    x: [Number],
+    y: [Number],
     speakText: [String],
     label: [String]
 }) {
@@ -18,14 +18,29 @@ class GridElement extends Model({
     toHTML() {
         return templates.getGridItem(this.label, this.width, this.height, this.x, this.y, this.id);
     };
+
+    hasSetPosition() {
+        return this.x != null && this.x != undefined && this.y != null && this.y != undefined;
+    }
+
+    static fromJSON(jsonData) {
+        var result = [];
+        var data = typeof jsonData === 'string' ? JSON.parse(jsonData): jsonData;
+        if(!(data instanceof Array)) {
+            data = [data];
+        }
+        data.forEach(function (item) {
+            result.push(new GridElement(item));
+        });
+
+        return result.length == 1 ? result[0] : result;
+    }
 }
 
 GridElement.defaults({
     id: "", //will be replaced by constructor
     width: 2,
-    height: 1,
-    x: 0,
-    y: 0
+    height: 1
 });
 
 var GridElementConverter = {
