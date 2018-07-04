2f807febf6fc74e86b740fd61f50d2f2121e7d09
Benjamin Klaus <klaus@technikum-wien.at>
2018-07-04 14:35:19 +0200

    auto-size and center images using flexbox, hack with background image in order to maintain aspect ratio

--- src/js/templates.js
+++ src/js/templates.js
@@ -16,11 +16,13 @@ templates.getGridItem = function (label, width, height, posX, posY, id) {
     posX = posX || 0;
     posY = posY || 0;
     id = id || lastId++;
+    var src = Math.random() < 0.5 ? "http://oxydy.com/wp-content/uploads/2018/02/test-img.png" : "http://www.wrench.at/img/CalendarMenu3/icon/appicon128x128@2x.png";
 
     var template = `
 <li class="item" data-w="${width}" data-h="${height}" data-x="${posX}" data-y="${posY}" data-id="${id}" data-label="${label}">
     <div class="grid-item-content" id="${id}" data-id="${id}">
-        ${label}
+        <div class="img-container" style="background-color: #777620; background:url(${src}) center no-repeat; background-size: contain;"/>
+        <div class="text-container"><span>${label}</span></div>
     </div>
 </li>`;
     return template;
