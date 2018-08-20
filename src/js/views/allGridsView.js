4e828a99bf6801ab4aacc02b54f6d8ad53c70602
Benjamin Klaus <klaus@technikum-wien.at>
2018-08-20 12:11:32 +0200

    added front-end methods to delete or add grid

--- src/js/views/allGridsView.js
+++ src/js/views/allGridsView.js
@@ -1,9 +1,12 @@
 import {dataService} from "../service/dataService";
+import {GridData} from "../model/GridData.js";
+import {GridElement} from "../model/GridElement.js";
 import Vue from 'vue'
 
 var AllGridsView = {};
 
-AllGridsView.init = function() {
+AllGridsView.init = function () {
+
     dataService.getGrids().then(grids => {
         console.log(grids);
         var app = new Vue({
@@ -11,6 +14,31 @@ AllGridsView.init = function() {
             data: {
                 grids: grids,
                 searchText: ''
+            },
+            methods: {
+                deleteGrid: function (id, label) {
+                    console.log('delete: ' + id)
+                    if(!confirm(`Do you really want to delete the grid "${label}"?`)) {
+                        return;
+                    }
+                    dataService.deleteGrid(id).then(() => {
+                        this.reload();
+                    });
+                },
+                addGrid: function () {
+                    console.log('add grid!');
+                    dataService.saveGrid(new GridData({
+                        label: 'test' + new Date().getTime(),
+                        gridElements: [new GridElement({label: 'Test 1'}), new GridElement({label: 'Test 2'})]
+                    })).then(() => {
+                        this.reload();
+                    });
+                },
+                reload: function () {
+                    dataService.getGrids().then(grids => {
+                        this.grids = grids;
+                    });
+                }
             },
             computed: {
                 filteredGrids() {
