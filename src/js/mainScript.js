24607a8c292e504a7d72b8a0e34ff2d5804abafe
Benjamin Klaus <klaus@technikum-wien.at>
2018-08-22 13:14:43 +0200

    removed fontawesome from js-import

--- src/js/mainScript.js
+++ src/js/mainScript.js
@@ -4,7 +4,6 @@ import {VueDirectives} from "./vue/directives";
 import './../css/custom.css';
 import './../css/gridlist.css';
 import './../css/jquery.contextMenu.css';
-import './../../node_modules/@fortawesome/fontawesome-free/css/all.css';
 
 function init() {
    Router.init('#content');
