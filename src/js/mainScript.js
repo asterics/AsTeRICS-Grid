34887adc7173bc12f434b87ed0f993db22c1b774
Benjamin Klaus <klaus@technikum-wien.at>
2018-08-22 11:55:12 +0200

    import jquery.contextmenu css

--- src/js/mainScript.js
+++ src/js/mainScript.js
@@ -3,6 +3,7 @@ import {VueDirectives} from "./vue/directives";
 
 import './../css/custom.css';
 import './../css/gridlist.css';
+import './../css/jquery.contextMenu.css';
 import './../../node_modules/@fortawesome/fontawesome-free/css/all.css';
 
 function init() {
