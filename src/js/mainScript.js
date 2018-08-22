b2500454c674a3df43abdedf8b93dfb05e89d880
Benjamin Klaus <klaus@technikum-wien.at>
2018-08-22 11:27:08 +0200

    import css files and fontawesome

--- src/js/mainScript.js
+++ src/js/mainScript.js
@@ -1,6 +1,10 @@
 import {Router} from "./router.js";
 import {VueDirectives} from "./vue/directives";
 
+import './../css/custom.css';
+import './../css/gridlist.css';
+import './../../node_modules/@fortawesome/fontawesome-free/css/all.css';
+
 function init() {
    Router.init('#content');
    VueDirectives.init();
