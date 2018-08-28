d06124669d0729e24aadb3a696673c0a3c28599e
Benjamin Klaus <klaus@technikum-wien.at>
2018-08-28 17:00:49 +0200

    added util module for transforming / encoding of images

---
+++ src/js/util/imageUtil.js
@@ -0,0 +1,43 @@
+var imageUtil = {};
+
+/**
+ * returns a base64 string that represents the image of the given img-element
+ * @param img the image element to convert
+ * @param maxWidth maximum width of the image
+ * @param quality quality of the image (0.0 - 1.0)
+ * @return {string}
+ */
+imageUtil.getBase64FromImg = function (img, maxWidth, quality) {
+    maxWidth = maxWidth || 150;
+    var canvas = document.createElement("canvas");
+    var factor = 1;
+    if (img.width > maxWidth) {
+        factor = maxWidth / img.width;
+    }
+    canvas.width = img.width * factor;
+    canvas.height = img.height * factor;
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    return canvas.toDataURL("image/jpeg", quality);
+};
+
+/**
+ * returns promise that resolves to a base64 string that represents the content of the file
+ * @param input the imput element to read the file from
+ * @return {Promise}
+ */
+imageUtil.getBase64FromInput = function (input) {
+    return new Promise(resolve => {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                resolve(e.target.result);
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    });
+};
+
+export {imageUtil};
