a8a5c2777e383a1e64d80269875551cfe0ce81d5
Benjamin Klaus <klaus@technikum-wien.at>
2018-08-28 17:00:23 +0200

    added model for grid image

---
+++ src/js/model/GridImage.js
@@ -0,0 +1,24 @@
+import {modelUtil} from "../util/modelUtil";
+
+class GridImage extends Model({
+    id: String,
+    modelName: String,
+    data: [String]
+}) {
+    constructor(properties, elementToCopy) {
+        properties = modelUtil.setDefaults(properties, elementToCopy, GridImage);
+        super(properties);
+        this.id = this.id || modelUtil.generateId('grid-image')
+    }
+
+    static getModelName() {
+        return "GridImage";
+    }
+}
+
+GridImage.defaults({
+    id: "", //will be replaced by constructor
+    modelName: GridImage.getModelName()
+});
+
+export {GridImage};
