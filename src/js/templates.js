afd1f350a48efab01c356f4c6d8da7ad83da7baa
Benjamin Klaus <klaus@technikum-wien.at>
2018-08-28 17:15:59 +0200

    adding dynamic image from model to template

--- src/js/templates.js
+++ src/js/templates.js
@@ -10,18 +10,23 @@ templates.getGridBase = function (gridId) {
 };
 
 
-templates.getGridItem = function (label, width, height, posX, posY, id) {
+templates.getGridItem = function (label, width, height, posX, posY, id, image) {
     width = width || 1;
     height = height || 1;
     posX = posX || 0;
     posY = posY || 0;
     id = id || lastId++;
-    var src = Math.random() < 0.5 ? "http://oxydy.com/wp-content/uploads/2018/02/test-img.png" : "http://www.wrench.at/img/CalendarMenu3/icon/appicon128x128@2x.png";
+    var imgData = Math.random() < 0.5 ? "http://oxydy.com/wp-content/uploads/2018/02/test-img.png" : "data:image/gif;base64,R0lGODdhMAAwAPAAAAAAAP///ywAAAAAMAAw AAAC8IyPqcvt3wCcDkiLc7C0qwyGHhSWpjQu5yqmCYsapyuvUUlvONmOZtfzgFz ByTB10QgxOR0TqBQejhRNzOfkVJ+5YiUqrXF5Y5lKh/DeuNcP5yLWGsEbtLiOSp a/TPg7JpJHxyendzWTBfX0cxOnKPjgBzi4diinWGdkF8kjdfnycQZXZeYGejmJl ZeGl9i2icVqaNVailT6F5iJ90m6mvuTS4OK05M0vDk0Q4XUtwvKOzrcd3iq9uis F81M1OIcR7lEewwcLp7tuNNkM3uNna3F2JQFo97Vriy/Xl4/f1cf5VWzXyym7PH hhx4dbgYKAAA7";;
+    var imgId = null;
+    if(image) {
+        imgData = image.data;
+        imgId = image.id;
+    }
 
     var template = `
-<li class="item" data-w="${width}" data-h="${height}" data-x="${posX}" data-y="${posY}" data-id="${id}" data-label="${label}">
+<li class="item" data-w="${width}" data-h="${height}" data-x="${posX}" data-y="${posY}" data-id="${id}" data-label="${label}" data-img-id="${imgId}" data-img="${imgData}">
     <div class="grid-item-content" id="${id}" data-id="${id}">
-        <div class="img-container" style="background-color: #777620; background:url(${src}) center no-repeat; background-size: contain;"/>
+        <div class="img-container" style="background-color: #777620; background: center no-repeat; background-size: contain; background-image: url('${imgData}')"/>
         <div class="text-container"><span>${label}</span></div>
     </div>
 </li>`;
