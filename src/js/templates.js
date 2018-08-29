98c1753657bb27b5093cc771b64e066a19824cf8
Benjamin Klaus <klaus@technikum-wien.at>
2018-08-29 11:23:13 +0200

    use empty string instead of null for non-existing id

--- src/js/templates.js
+++ src/js/templates.js
@@ -17,7 +17,7 @@ templates.getGridItem = function (label, width, height, posX, posY, id, image) {
     posY = posY || 0;
     id = id || lastId++;
     var imgData = Math.random() < 0.5 ? "http://oxydy.com/wp-content/uploads/2018/02/test-img.png" : "data:image/gif;base64,R0lGODdhMAAwAPAAAAAAAP///ywAAAAAMAAw AAAC8IyPqcvt3wCcDkiLc7C0qwyGHhSWpjQu5yqmCYsapyuvUUlvONmOZtfzgFz ByTB10QgxOR0TqBQejhRNzOfkVJ+5YiUqrXF5Y5lKh/DeuNcP5yLWGsEbtLiOSp a/TPg7JpJHxyendzWTBfX0cxOnKPjgBzi4diinWGdkF8kjdfnycQZXZeYGejmJl ZeGl9i2icVqaNVailT6F5iJ90m6mvuTS4OK05M0vDk0Q4XUtwvKOzrcd3iq9uis F81M1OIcR7lEewwcLp7tuNNkM3uNna3F2JQFo97Vriy/Xl4/f1cf5VWzXyym7PH hhx4dbgYKAAA7";;
-    var imgId = null;
+    var imgId = '';
     if(image) {
         imgData = image.data;
         imgId = image.id;
