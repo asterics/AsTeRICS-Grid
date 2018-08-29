85c90f2d719972cff528e3b35733e0d29bdad807
Benjamin Klaus <klaus@technikum-wien.at>
2018-08-29 12:01:09 +0200

    remove test images, make default image empty

--- src/js/templates.js
+++ src/js/templates.js
@@ -16,7 +16,7 @@ templates.getGridItem = function (label, width, height, posX, posY, id, image) {
     posX = posX || 0;
     posY = posY || 0;
     id = id || lastId++;
-    var imgData = Math.random() < 0.5 ? "http://oxydy.com/wp-content/uploads/2018/02/test-img.png" : "data:image/gif;base64,R0lGODdhMAAwAPAAAAAAAP///ywAAAAAMAAw AAAC8IyPqcvt3wCcDkiLc7C0qwyGHhSWpjQu5yqmCYsapyuvUUlvONmOZtfzgFz ByTB10QgxOR0TqBQejhRNzOfkVJ+5YiUqrXF5Y5lKh/DeuNcP5yLWGsEbtLiOSp a/TPg7JpJHxyendzWTBfX0cxOnKPjgBzi4diinWGdkF8kjdfnycQZXZeYGejmJl ZeGl9i2icVqaNVailT6F5iJ90m6mvuTS4OK05M0vDk0Q4XUtwvKOzrcd3iq9uis F81M1OIcR7lEewwcLp7tuNNkM3uNna3F2JQFo97Vriy/Xl4/f1cf5VWzXyym7PH hhx4dbgYKAAA7";;
+    var imgData = '';
     var imgId = '';
     if(image) {
         imgData = image.data;
