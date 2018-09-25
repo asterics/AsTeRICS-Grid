2ac73303c5929f097099dadf439b3cc667aab426
Benjamin Klaus <klaus@technikum-wien.at>
2018-09-25 11:17:51 +0200

    style changes: center text on grid items if there is no image

--- src/js/templates.js
+++ src/js/templates.js
@@ -18,16 +18,19 @@ templates.getGridItem = function (label, width, height, posX, posY, id, image) {
     id = id || lastId++;
     var imgData = '';
     var imgId = '';
+    var txtContainerStyle = '';
     if(image) {
         imgData = image.data;
         imgId = image.id;
+    } else {
+        txtContainerStyle = 'flex: 1 1 auto;';
     }
 
     var template = `
 <li class="item" data-w="${width}" data-h="${height}" data-x="${posX}" data-y="${posY}" data-id="${id}" data-label="${label}" data-img-id="${imgId}" data-img="${imgData}">
     <div class="grid-item-content" id="${id}" data-id="${id}">
         <div class="img-container" style="background-color: #777620; background: center no-repeat; background-size: contain; background-image: url('${imgData}')"/>
-        <div class="text-container"><span>${label}</span></div>
+        <div class="text-container" style="${txtContainerStyle}"><span>${label}</span></div>
     </div>
 </li>`;
     return template;
