8f74a5faa29d31e4cbc27d18fef26a1a37fc0423
Benjamin Klaus <klaus@technikum-wien.at>
2018-10-03 17:03:08 +0200

    no margin in image container if no image

--- src/js/templates.js
+++ src/js/templates.js
@@ -21,18 +21,20 @@ templates.getGridItem = function (label, width, height, posX, posY, id, image) {
     label = label || "";
     var imgData = '';
     var imgId = '';
-    var txtContainerStyle = 'font-size: ' + fontUtil.getFontSizePercent(label, width, height, image) + ';';
+    var txtContainerStyle = '';
+    var imgContainerMargin = '3%';
     if(image) {
         imgData = image.data;
         imgId = image.id;
     } else {
-        txtContainerStyle += 'flex: 1 1 auto;';
+        txtContainerStyle = 'flex: 1 1 auto;';
+        imgContainerMargin = '0'
     }
 
     var template = `
 <li class="item" data-w="${width}" data-h="${height}" data-x="${posX}" data-y="${posY}" data-id="${id}" data-label="${label}" data-img-id="${imgId}" data-img="${imgData}">
     <div class="grid-item-content" id="${id}" data-id="${id}">
-        <div class="img-container" style="background-color: #777620; background: center no-repeat; background-size: contain; background-image: url('${imgData}')"/>
+        <div class="img-container" style="background-color: #777620; background: center no-repeat; background-size: contain; background-image: url('${imgData}'); margin: ${imgContainerMargin};"/>
         <div class="text-container" style="${txtContainerStyle}"><span>${label}</span></div>
     </div>
 </li>`;
