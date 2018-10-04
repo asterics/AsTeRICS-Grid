2e1c295c626dd85d7a6eed0231d0491639b2b5b5
Benjamin Klaus <klaus@technikum-wien.at>
2018-10-04 11:30:54 +0200

    do not print whole element on doing an action

--- src/js/service/actionService.js
+++ src/js/service/actionService.js
@@ -3,7 +3,7 @@ import {dataService} from "./dataService";
 var actionService = {
     doAction: function (gridId, gridElementId) {
         dataService.getGridElement(gridId, gridElementId).then(gridElement => {
-            console.log('do action for: ' + JSON.stringify(gridElement));
+            console.log('do action for: ' + gridElement.label);
             if(typeof SpeechSynthesisUtterance !== 'undefined') {
                 var msg = new SpeechSynthesisUtterance(gridElement.label);
                 window.speechSynthesis.speak(msg);
