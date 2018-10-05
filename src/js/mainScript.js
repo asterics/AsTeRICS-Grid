d2ac28553c05dacd6085e0712ddfbf90821e58ac
Benjamin Klaus <klaus@technikum-wien.at>
2018-10-05 12:41:41 +0200

    added own css for allgridsview

--- src/js/mainScript.js
+++ src/js/mainScript.js
@@ -4,6 +4,7 @@ import {VueDirectives} from "./vue/directives";
 import './../css/custom.css';
 import './../css/gridlist.css';
 import './../css/jquery.contextMenu.css';
+import './../css/allGridsView.css';
 
 function init() {
    Router.init('#content');
