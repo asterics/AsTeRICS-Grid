3feef0ac8433389f0cd76f5f37b890119913f5e8
Benjamin Klaus <klaus@technikum-wien.at>
2018-10-10 14:01:05 +0200

    debugging appcache events

--- src/js/mainScript.js
+++ src/js/mainScript.js
@@ -15,9 +15,36 @@ init();
 
 function reloadOnAppcacheUpdate() {
     function onUpdateReady() {
-        window.location.reload();
+        console.log('appcache: updateready');
+        //window.location.reload();
     }
+
     window.applicationCache.addEventListener('updateready', onUpdateReady);
+    window.applicationCache.addEventListener('checking', function () {
+        console.log('appcache: checking');
+    });
+    window.applicationCache.addEventListener('downloading', function () {
+        console.log('appcache: downloading');
+    });
+    window.applicationCache.addEventListener('progress', function () {
+        console.log('appcache: progress');
+    });
+    window.applicationCache.addEventListener('error', function () {
+        console.log('appcache: error');
+    });
+    window.applicationCache.addEventListener('obsolete', function () {
+        console.log('appcache: obsolete');
+    });
+    window.applicationCache.addEventListener('cached', function () {
+        console.log('appcache: cached');
+    });
+    window.applicationCache.addEventListener('noupdate', function () {
+        console.log('appcache: noupdate');
+    });
+    window.applicationCache.addEventListener('updateready', function () {
+        console.log('appcache: updateready');
+    });
+
     if (window.applicationCache.status === window.applicationCache.UPDATEREADY) {
         onUpdateReady();
     }
