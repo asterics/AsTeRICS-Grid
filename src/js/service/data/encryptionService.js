4267ad63e680602bbbc87f9334257879019e58c8
Benjamin Klaus <klaus@technikum-wien.at>
2019-01-04 10:53:29 +0100

    moved encryption to extra methods which encrypt/decrypt strings

--- src/js/service/data/encryptionService.js
+++ src/js/service/data/encryptionService.js
@@ -11,18 +11,22 @@ let _encryptionKey = null;
  * @see{EncryptedObject}
  *
  * @param object any model object to encrypt
+ * @param encryptionKey the key that should be used for encryption (optional, local _encryptionKey variable is used
+ *        if not set)
  * @return {*} encrypted object of type @see{EncryptedObject}
  */
 encryptionService.encryptObject = function (object, encryptionKey) {
-    if(!object) {
+    if (!object) {
         return object;
     }
+
     let encryptedObject = new EncryptedObject({
         id: object.id,
         modelName: object.modelName
     });
     encryptedObject._id = object.id;
-    encryptedObject.encryptedDataBase64 = btoa(JSON.stringify(object));
+    let jsonString = JSON.stringify(object);
+    encryptedObject.encryptedDataBase64 = encryptionService.encryptString(jsonString, encryptionKey);
     return encryptedObject;
 };
 
@@ -32,21 +36,59 @@ encryptionService.encryptObject = function (object, encryptionKey) {
  *
  * @param encryptedObjects an array of encrypted objects or a single encrypted object
  * @param objectType the type of the objects to decrypt
+ * @param encryptionKey the key that should be used for decryption (optional, local _encryptionKey variable is used
+ *        if not set)
  * @return {*} an array or single object (depending on input) of decrypted instances of objects of type "objectType"
  */
-encryptionService.decryptObjects = function (encryptedObjects, objectType) {
-    if(!encryptedObjects) {
+encryptionService.decryptObjects = function (encryptedObjects, objectType, encryptionKey) {
+    if (!encryptedObjects) {
         return encryptedObjects;
     }
-    encryptedObjects = encryptedObjects instanceof Array ? encryptedObjects: [encryptedObjects];
+
+    encryptedObjects = encryptedObjects instanceof Array ? encryptedObjects : [encryptedObjects];
     let decryptedObjects = [];
     encryptedObjects.forEach(encryptedObject => {
-       let decryptedObject = JSON.parse(atob(encryptedObject.encryptedDataBase64));
-       decryptedObject = objectType ? new objectType(decryptedObject) : decryptedObject;
-       decryptedObjects.push(decryptedObject);
+        let decryptedString = encryptionService.decryptString(encryptedObject.encryptedDataBase64, encryptionKey);
+        let decryptedObject = JSON.parse(decryptedString);
+        decryptedObject = objectType ? new objectType(decryptedObject) : decryptedObject;
+        decryptedObjects.push(decryptedObject);
     });
 
     return decryptedObjects.length > 1 ? decryptedObjects : decryptedObjects[0];
+};
+
+/**
+ * encrypts a string and returns a base64 representation of the encrypted data.
+ *
+ * @param string the string to encrypt
+ * @param encryptionKey the key that should be used for encryption (optional, local _encryptionKey variable is used
+ *        if not set). If no encryption key is set in both, this method just encodes the given string to base64.
+ * @return {string} the given string in encrypted form, encoded in base64
+ */
+encryptionService.encryptString = function (string, encryptionKey) {
+    encryptionKey = encryptionKey || _encryptionKey;
+    let encryptedString = string;
+    if (encryptionKey) {
+        //TODO encrypt
+    }
+    return btoa(encryptedString);
+};
+
+/**
+ * decrypts a given base64 encoded string.
+ *
+ * @param encryptedString a base64 encoded string that was encrypted before
+ * @param encryptionKey the key that should be used for decryption (optional, local _encryptionKey variable is used
+ *        if not set). If no encryption key is set in both, this method just decodes a given base64 encoded string.
+ * @return {string} the decrypted string, not base64 encoded
+ */
+encryptionService.decryptString = function (encryptedString, encryptionKey) {
+    encryptionKey = encryptionKey || _encryptionKey;
+    let decryptedString = atob(encryptedString);
+    if (encryptionKey) {
+        //TODO decrypt
+    }
+    return decryptedString;
 };
 
 /**
