f3926b22b6fbfc61f687954ee2867380cfda18ba
Benjamin Klaus <klaus@technikum-wien.at>
2019-01-09 16:17:26 +0100

    added dataUtil for removing binary data from objects and test cases for it

---
+++ src/js/util/dataUtil.js
@@ -0,0 +1,55 @@
+let dataUtil = {};
+
+let defaultRemovedPlaceholder = '_removed_';
+
+/**
+ * Recursively iterates over all properties of a given object and removes all String values longer than a given threshold.
+ * Does not modifiy the original object.
+ *
+ * @param object the object to process
+ * @param maxLength maximum length of the String properties, everything longer is replaced by 'removedPlaceholder'
+ * @param removedPlaceholder value that should be used as replacement for too long values (optional, defaults to '_removed_')
+ * @return {*} copy of given object with String values that are longer than maxLength replaced by removedPlaceholder
+ */
+dataUtil.removeLongPropertyValues = function(object, maxLength, removedPlaceholder) {
+    if(!object) {
+        return object;
+    }
+    removedPlaceholder = removedPlaceholder === undefined ? defaultRemovedPlaceholder: removedPlaceholder;
+    let copy = JSON.parse(JSON.stringify(object));
+    Object.keys(copy).forEach(key => {
+        copy[key] = shortenObjectInternal(copy[key], maxLength, removedPlaceholder);
+    });
+    return copy;
+};
+
+/**
+ * returns the default placeholder that is used for 'removeLongPropertyValues' if no
+ * parameter is passed
+ * @return {string}
+ */
+dataUtil.getDefaultRemovedPlaceholder = function() {
+    return defaultRemovedPlaceholder;
+};
+
+function shortenObjectInternal(object, maxLength, removedPlaceholder) {
+    if(!object) {
+        return object;
+    }
+    if(typeof object === 'string' || object instanceof String) {
+        if(object.length > maxLength) {
+            return removedPlaceholder;
+        }
+    } else if(object instanceof Array) {
+        for(let i = 0; i<object.length; i++) {
+            object[i] = shortenObjectInternal(object[i], maxLength, removedPlaceholder);
+        }
+    } else {
+        Object.keys(object).forEach(nextKey => {
+            object[nextKey] = shortenObjectInternal(object[nextKey], maxLength, removedPlaceholder);
+        });
+    }
+    return object;
+}
+
+export {dataUtil};
