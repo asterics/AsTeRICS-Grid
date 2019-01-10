0a1fa2b4f6d09b172489fb0d7e2394644ddfdf66
Benjamin Klaus <klaus@technikum-wien.at>
2019-01-10 11:22:11 +0100

    added short version of encrypted data excluding base64 content

--- src/js/model/EncryptedObject.js
+++ src/js/model/EncryptedObject.js
@@ -3,7 +3,8 @@ import {modelUtil} from "../util/modelUtil";
 class EncryptedObject extends Model({
     id: String, //ID of the encrypted object
     modelName: String, //modelName of the encrypted object
-    encryptedDataBase64: [String] //stored object, serialized as JSON, encrypted and afterwards base64 encoded
+    encryptedDataBase64: [String], //stored object, serialized as JSON, encrypted and afterwards base64 encoded
+    encryptedDataBase64Short: [String] //same as encryptedDataBase64, but with removed long values (no binary base64 strings)
 }) {
     constructor(properties, elementToCopy) {
         properties = modelUtil.setDefaults(properties, elementToCopy, EncryptedObject);
