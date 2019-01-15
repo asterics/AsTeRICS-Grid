eefdec606adc856781b8b2b9bd04a736afe686c8
Benjamin Klaus <klaus@technikum-wien.at>
2019-01-15 16:39:53 +0100

    fixed model version constant to be valid JSON

--- src/js/util/constants.js
+++ src/js/util/constants.js
@@ -1,6 +1,6 @@
 var constants = {};
 
 constants.ELEMENT_EVENT_ID = "ELEMENT_EVENT_ID";
-constants.MODEL_VERSION = "{major: 1, minor: 0, patch: 0}";
+constants.MODEL_VERSION = '{"major": 1, "minor": 0, "patch": 0}';
 
 export {constants};
