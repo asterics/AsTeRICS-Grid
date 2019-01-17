bfb955d90cb025c955dede6e3799472f0e01c428
Benjamin Klaus <klaus@technikum-wien.at>
2019-01-17 15:38:11 +0100

    added release script for automatic releases + tagging

---
+++ scripts/release.sh
@@ -0,0 +1,32 @@
+tagname="release-$(date +%Y-%m-%d-%H:%M/%z)"
+echo "building..."
+npm run build
+echo "commiting bundles and manifest..."
+git add package/static/build
+git add package/static/build_legacy
+git add package/static/manifest.appcache
+git commit -m "added bundles and appcache for release $tagname"
+echo "creating tag '$tagname'..."
+git tag -a $tagname
+git push origin $tagname
+if git diff-index --quiet HEAD --; then
+    # No changes
+    echo "no local changes, apply release to gh-pages..."
+    git checkout gh-pages
+    git merge master
+    git push origin gh-pages
+    git checkout master
+else
+    # Changes
+    echo "detected local changes, doing git stash..."
+    git stash
+    echo "apply release to gh-pages..."
+    git checkout gh-pages
+    git merge master
+    git push origin gh-pages
+    git checkout master
+    git stash pop
+fi
+echo "$tagname successfully released!"
+
+
