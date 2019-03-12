0912e8f8197cd3746d6eb8e4d55182538575fab5
Benjamin Klaus <klaus@technikum-wien.at>
2019-03-12 14:46:34 +0100

    added debounce directive

--- src/js/vue/directives.js
+++ src/js/vue/directives.js
@@ -1,12 +1,24 @@
 import Vue from 'vue'
 import {translateService} from "../service/translateService";
 
+let timeoutID = null;
+
 var VueDirectives = {
     init: () => {
         Vue.directive('focus', {
             inserted: function (el) {
                 if(el.focus) el.focus();
                 if(el.select) el.select();
+            }
+        });
+        Vue.directive('debounce', {
+            inserted: function (el, binding) {
+                el.oninput = function (evt) {
+                    clearTimeout(timeoutID);
+                    timeoutID = setTimeout(function () {
+                        el.dispatchEvent(new Event('change'))
+                    }, parseInt(binding.value) || 500);
+                }
             }
         });
         Vue.filter('translate', function (key) {
