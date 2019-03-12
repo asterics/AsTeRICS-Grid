d7ba2a0b0b3a39eeab539d7c5f01523e3587e195
Benjamin Klaus <klaus@technikum-wien.at>
2019-03-12 16:21:10 +0100

    added possibility to save/get/remove user passwords

--- src/js/service/data/localStorageService.js
+++ src/js/service/data/localStorageService.js
@@ -1,7 +1,7 @@
 var errorMsg = 'could not access local storage, maybe disabled by user? Error: ';
 var storage = null;
 let FIRST_VISIT_KEY = 'FIRST_VISIT_KEY';
-let USER_PASSWORD = "USER_PASSWORD";
+let USER_PASSWORDS_KEY = "USER_PASSWORDS_KEY";
 
 if (typeof(Storage) !== "undefined") {
     try {
@@ -30,6 +30,15 @@ var localStorageService = {
             }
         }
     },
+    remove: function (key) {
+        if (storage) {
+            try {
+                return storage.removeItem(key);
+            } catch (e) {
+                log.error(errorMsg + e)
+            }
+        }
+    },
     /**
      * returns true if the page was never visited before and this method was never called before.
      * returns false afterwards.
@@ -42,20 +51,47 @@ var localStorageService = {
     },
     /**
      * returns a previously saved user password
+     * @param username the username as used for login
      * @return {*}
      */
-    getUserPassword() {
-        return localStorageService.get(USER_PASSWORD);
+    getUserPassword(username) {
+        return getPasswordObject()[username];
     },
     /**
      * saves a given user password
+     * @param username the username as used for login
      * @param password the password to save (should be salted + hashed)
-     * @return {boolean}
      */
-    saveUserPassword(password) {
-        let value = localStorageService.save(USER_PASSWORD, password);
-        return !value;
+    saveUserPassword(username, password) {
+        let object = getPasswordObject();
+        object[username] = password;
+        localStorageService.save(USER_PASSWORDS_KEY, JSON.stringify(object));
+    },
+    /**
+     * removes a saved password of a given user from local storage
+     * @param username the username as used for login
+     */
+    removeUserPassword(username) {
+        let object = getPasswordObject();
+        delete object[username];
+        localStorageService.save(USER_PASSWORDS_KEY, JSON.stringify(object));
+    },
+    /**
+     * returns all users with saved passwords as a string list
+     */
+    getSavedUsers() {
+        let object = getPasswordObject();
+        return Object.keys(object);
     }
 };
+
+function getPasswordObject() {
+    let passwordsObjectString = localStorageService.get(USER_PASSWORDS_KEY);
+    if (!passwordsObjectString) {
+        localStorageService.save(USER_PASSWORDS_KEY, JSON.stringify({}));
+        return {};
+    }
+    return JSON.parse(passwordsObjectString);
+}
 
 export {localStorageService};
