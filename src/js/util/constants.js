c4ba5e88aa68f3ee765e27e9be9f7f6ad4e7c8bf
Benjamin Klaus <klaus@technikum-wien.at>
2019-04-01 16:38:17 +0200

    added constants for event names

--- src/js/util/constants.js
+++ src/js/util/constants.js
@@ -5,4 +5,7 @@ constants.MODEL_VERSION = '{"major": 1, "minor": 0, "patch": 0}';
 
 constants.LOCAL_NOLOGIN_USERNAME = 'local-default-user';
 
+constants.EVENT_DB_CONNECTION_LOST = 'EVENT_DB_CONNECTION_LOST';
+constants.EVENT_DB_PULL_UPDATED = 'EVENT_DB_PULL_UPDATED';
+
 export {constants};
