3b635879ddaa419d7a7c7ea65079958baa32b0d1
Benjamin Klaus <klaus@technikum-wien.at>
2019-04-03 15:19:58 +0200

    added log.trace() to mock

--- src/js/util/__mocks__/log.js
+++ src/js/util/__mocks__/log.js
@@ -5,6 +5,7 @@ log.error = function () { logInternal(arguments); };
 log.warn = function () { logInternal(arguments); };
 log.info = function () { logInternal(arguments); };
 log.debug = function () { logInternal(arguments); };
+log.trace = function () { logInternal(arguments); };
 
 function logInternal(args) {
     if(enabled) {
