0cc00a92f3d5bc8ae101d49b0e1b1cda6247cc15
Benjamin Klaus <klaus@technikum-wien.at>
2019-04-25 10:59:37 +0200

    added beta-release script, introduced function do_gh_pages_update

--- scripts/releaseBeta.sh
+++ scripts/releaseBeta.sh
@@ -1,36 +1,31 @@
 set -e
 
 # ------------------------------------------------------------------
-# AsTeRICS Grid release script
+# AsTeRICS Grid beta-release script
 # ------------------------------------------------------------------
 # releases the committed version on the current branch to gh-pages
-# by performing the following steps:
+# as beta-release by performing the following steps:
 # 1) a new tag named after the current date/time is created an published
-# 2) the version of the new tag is pushed to the gh-pages branch both
-#    as the normal version and the beta version.
+# 2) the version of the new tag is pushed to the gh-pages branch in the
+#    folder/subpath "beta". The main release remains the same.
 # 3) the original branch is checked out
 # 4) if there are uncommitted changes on the current branch they are stashed
 #    before and popped after the release
-#
-# Note: if you are using this script to release a bugfix it's necessary to manually
-# restore the original beta-version afterwards since the beta-version will be the same
-# as the bug-fixed version after releasing.
 
 do_gh_pages_update () {
-    echo "apply release to gh-pages..."
-    git checkout gh-pages
-    git reset --hard $tagname
-    rm -rf beta
-    git clone --branch $tagname https://github.com/asterics/AsTeRICS-Grid.git beta
-    git add beta
-    git commit -m "added tag '$tagname' for beta version in folder beta."
-    git push origin gh-pages -f
-    git checkout $branch
+   echo "apply beta-release to gh-pages..."
+   git checkout gh-pages
+   rm -rf beta
+   git clone --branch $tagname https://github.com/asterics/AsTeRICS-Grid.git beta
+   git add beta
+   git commit -m "added tag '$tagname' for beta version in folder beta."
+   git push origin gh-pages -f
+   git checkout $branch
 }
 
 branch=$(git symbolic-ref --short HEAD)
-tagname="release-$(date +%Y-%m-%d-%H.%M/%z)"
-tagnameSed="release-$(date +%Y-%m-%d-%H.%M\\/%z)"
+tagname="release-beta-$(date +%Y-%m-%d-%H.%M/%z)"
+tagnameSed="release-beta-$(date +%Y-%m-%d-%H.%M\\/%z)"
 echo $tagnameSed
 sed -i -e "s/#ASTERICS_GRID_VERSION#/$tagnameSed/g" src/js/mainScript.js
 
@@ -40,7 +35,7 @@ echo "commiting bundles and manifest..."
 git add package/static/build
 git add package/static/build_legacy
 git add package/static/manifest.appcache
-git commit -m "added bundles and appcache for release $tagname"
+git commit -m "added bundles and appcache for beta-release $tagname"
 git push origin HEAD
 git checkout src/js/mainScript.js
 echo "creating tag '$tagname'..."
@@ -57,6 +52,6 @@ else
     do_gh_pages_update
     git stash pop
 fi
-echo "$tagname successfully released!"
+echo "beta-release $tagname successfully released!"
 
 
