f77c4e57f484277fb867a31c91e0f7d16701c82f
Benjamin Klaus <klaus@technikum-wien.at>
2019-05-09 11:34:03 +0200

    added class MapCache - simple cache for caching encryption results

---
+++ src/js/util/MapCache.js
@@ -0,0 +1,96 @@
+/**
+ * simple cache, storing key-value pairs while ensuring that
+ * 1) stored values cannot be changed from any other point of execution -> stored as JSON string
+ * 2) each returned value is a deep copy of the original value -> also prevents errors of remotely changed objects
+ * @constructor
+ */
+function MapCache() {
+    let thiz = this;
+    let _cache = {};
+    let _objectTypeMap = {};
+
+    /**
+     * stores a key-value pair
+     * @param key the key, has to be a string
+     * @param value the value, any JSON-serializable object
+     * @param objectType optional constructor function, if defined the MapCache.get() will return new constructor(value)
+     */
+    thiz.set = function (key, value, objectType) {
+        if (typeof key !== 'string') {
+            log.warn('cache-key has to be a string, aborting.');
+            return;
+        }
+        if (!value) {
+            log.warn('cache-values has to be set, aborting.');
+            return;
+        }
+        if (objectType && typeof objectType !== 'function') {
+            log.warn('object type has to be a constructor function or empty, aborting.');
+            return;
+        }
+        _cache[key] = JSON.stringify(value);
+        if (objectType) {
+            _objectTypeMap[key] = objectType;
+        }
+    };
+
+    /**
+     * returns a saved value for a key.
+     * @param key the key to retrieve from cache
+     * @return {*} a deep copy of the original value stored with MapCache.set().
+     *         If 'objectType' parameter was set at calling MapCache.set(), the returned value will be new constructor(value)
+     *         If 'objectType' parameter was set at calling MapCache.set() and the original value was an array,
+     *         the returned value will be [new constructor(originalValue[0]), new constructor(originalValue[1]), ...]
+     */
+    thiz.get = function (key) {
+        if (typeof key !== 'string') {
+            log.warn('cache-key has to be a string, aborting.');
+            return;
+        }
+        if (!_cache[key]) {
+            return null;
+        }
+        let value = JSON.parse(_cache[key]);
+        let objectType = _objectTypeMap[key];
+        let isArray = value instanceof Array;
+        return isArray ? value.map(e => instantiate(objectType, e)) : instantiate(objectType, value);
+    };
+
+    /**
+     * returns true if the given key is stored in the cache
+     * @param key
+     * @return {boolean}
+     */
+    thiz.has = function (key) {
+        if (typeof key !== 'string') {
+            log.warn('cache-key has to be a string, aborting.');
+            return;
+        }
+        return !!_cache[key];
+    };
+
+    /**
+     * removes a single item from cache by key
+     * @param key
+     */
+    thiz.clear = function (key) {
+        if (key) {
+            delete _cache[key];
+            delete _objectTypeMap[key];
+        }
+    };
+
+    /**
+     * removes all items from cache
+     */
+    thiz.clearAll = function () {
+        _cache = {};
+        _objectTypeMap = {};
+    };
+
+    function instantiate(constructor, param) {
+        return constructor ? new constructor(param) : param;
+    }
+}
+
+export {MapCache};
