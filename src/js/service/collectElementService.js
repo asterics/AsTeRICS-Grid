34077c5d5e3f905dbe3f2efbb8f2d1d7a47e15fb
Benjamin Klaus <klaus@technikum-wien.at>
2019-05-31 16:18:08 +0200

    add event listener for applying predictions from PREDICTION elements, call predictionService.predict if there is a GridActionPredict action defined for the collectElement or if text changed and suggestOnChange === true

--- src/js/service/collectElementService.js
+++ src/js/service/collectElementService.js
@@ -2,6 +2,7 @@ import {GridElement} from "../model/GridElement";
 import {speechService} from "./speechService";
 import {constants} from "./../util/constants";
 import {translateService} from "./translateService";
+import {predictionService} from "./predictionService";
 
 var collectElementService = {};
 
@@ -11,15 +12,18 @@ var collectedTexts = {};
 collectElementService.initWithElements = function (elements) {
     collectElementService.reset();
     elements.forEach(element => {
-        if(element && element.type == GridElement.ELEMENT_TYPE_COLLECT) {
+        if (element && element.type == GridElement.ELEMENT_TYPE_COLLECT) {
             registeredCollectElements.push(JSON.parse(JSON.stringify(element)));
         }
     });
 };
 
-collectElementService.doAction = function (collectElemId) {
-    var collectElem = getCollectElem(collectElemId);
-    var collectedText = getText(collectElemId);
+collectElementService.doAction = function (elem) {
+    let collectElem = getCollectElem(elem.id);
+    let collectedText = getText(elem.id);
+    if (getActionOfType(elem, 'GridActionPredict')) {
+        predictionService.predict(collectedText);
+    }
     speechService.speak(collectedText, translateService.getLang());
 };
 
@@ -28,9 +32,20 @@ collectElementService.reset = function () {
     collectedTexts = {};
 };
 
+function getActionOfType(elem, type) {
+    if (!elem) {
+        return null;
+    }
+    let index = elem.actions.map(action => action.modelName).indexOf(type);
+    if (index === -1) {
+        return null;
+    }
+    return elem.actions[index];
+}
+
 function addText(elemId, text) {
     collectedTexts[elemId] = collectedTexts[elemId] || '';
-    if(text.length > 1 && collectedTexts[elemId]) {
+    if (text.length > 1 && collectedTexts[elemId]) {
         collectedTexts[elemId] += ' ' + text;
     } else {
         collectedTexts[elemId] += text;
@@ -45,11 +60,30 @@ function getCollectElem(elemId) {
     return registeredCollectElements.filter(el => el.id == elemId)[0];
 }
 
-$(window).on(constants.ELEMENT_EVENT_ID, function(event, element) {
-    if(!element.type || element.type == GridElement.ELEMENT_TYPE_NORMAL) {
+$(window).on(constants.ELEMENT_EVENT_ID, function (event, element) {
+    if (!element.type || element.type === GridElement.ELEMENT_TYPE_NORMAL) {
         registeredCollectElements.forEach(collectElem => {
             addText(collectElem.id, element.label);
-            $(`#${collectElem.id} textarea`)[0].value = getText(collectElem.id);
+            let text = getText(collectElem.id);
+            $(`#${collectElem.id} textarea`)[0].value = text;
+            let predictAction = getActionOfType(collectElem, 'GridActionPredict');
+            if (predictAction.suggestOnChange) {
+                predictionService.predict(text);
+            }
+        });
+    }
+    if (!element.type || element.type === GridElement.ELEMENT_TYPE_PREDICTION) {
+        registeredCollectElements.forEach(collectElem => {
+            let word = $(`#${element.id} .text-container span`).text();
+            if (word) {
+                let appliedText = predictionService.applyPrediction(collectedTexts[collectElem.id] || '', word);
+                collectedTexts[collectElem.id] = appliedText;
+                $(`#${collectElem.id} textarea`)[0].value = getText(collectElem.id);
+                let predictAction = getActionOfType(collectElem, 'GridActionPredict');
+                if (predictAction.suggestOnChange) {
+                    predictionService.predict(appliedText);
+                }
+            }
         });
     }
 });
