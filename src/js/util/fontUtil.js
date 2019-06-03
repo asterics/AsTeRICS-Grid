442899f6ecfb30116ef37c0d997f3601ec3ab6ba
Benjamin Klaus <klaus@technikum-wien.at>
2019-06-03 10:43:29 +0200

    adapted automatic font size calculation for textarea in collection elements

--- src/js/util/fontUtil.js
+++ src/js/util/fontUtil.js
@@ -6,8 +6,8 @@ var lastSize = '20px';
  * @param elem
  * @return {*}
  */
-fontUtil.getFontSizePx = function (elem) {
-    var label = elem.attr('data-label') || $(elem).find('.text-container span').text();
+fontUtil.getFontSizePx = function (elem, oneLine) {
+    var label = getLabel(elem);
     var imageId = elem.attr('data-img-id');
     if (!label) {
         return "10px";
@@ -16,8 +16,9 @@ fontUtil.getFontSizePx = function (elem) {
     var rectElem = elem[0].getBoundingClientRect();
     var areaElem = rectElem.height * rectElem.width / (imageId ? 2 : 1);
     var fontSize1 = Math.floor(Math.sqrt(areaElem * 0.5 / Math.max(15, label.length)));
-    var longestWordLength = Math.max.apply(null, label.split(' ').map(elem => elem.length));
+    var longestWordLength = oneLine ? label.length : Math.max.apply(null, label.split(' ').map(elem => elem.length));
     var fontSize2 = 1.4 * rectElem.width / longestWordLength;
+    fontSize2 = !oneLine ? fontSize2 : Math.min(fontSize2, 30);
     lastSize = Math.min(fontSize1, fontSize2) + "px";
     return lastSize;
 };
@@ -27,10 +28,15 @@ fontUtil.getFontSizePx = function (elem) {
  */
 fontUtil.adaptFontSize = function (elems) {
     for (var i = 0; i < elems.length; i++) {
+        let oneLine = false;
         var elem = elems[i];
         var textContainerElem = $(elem).find('.text-container')[0];
+        if (!textContainerElem) {
+            textContainerElem = $(elem).find('textarea')[0];
+            oneLine = true;
+        }
         if (textContainerElem) {
-            textContainerElem.style.fontSize = fontUtil.getFontSizePx($(elem));
+            textContainerElem.style.fontSize = fontUtil.getFontSizePx($(elem), oneLine);
         }
     }
 
@@ -49,5 +55,18 @@ fontUtil.adaptFontSizeForGridElements = function () {
 fontUtil.getLastFontSize = function () {
     return lastSize;
 };
+
+function getLabel(elem) {
+    if (elem.attr('data-label')) {
+        return elem.attr('data-label');
+    }
+    if ($(elem).find('.text-container span').text()) {
+        return $(elem).find('.text-container span').text()
+    }
+    if ($(elem).find('textarea')[0]) {
+        return $(elem).find('textarea')[0].value;
+    }
+    return "";
+}
 
 export {fontUtil};
