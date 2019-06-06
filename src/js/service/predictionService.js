003733ca478fd7579ebb53ffd44238577d411c53
Benjamin Klaus <klaus@technikum-wien.at>
2019-06-06 15:57:48 +0200

    added method learnFromInput, only save dictionaries that were changed

--- src/js/service/predictionService.js
+++ src/js/service/predictionService.js
@@ -9,19 +9,32 @@ let predictionary = null;
 let registeredPredictElements = [];
 let _dbDictObjects = [];
 let _unsavedChanges = false;
+let _usedKeys = [];
 
 predictionService.predict = function (input, dictionaryKey) {
     initIfUnititialized();
     if (!dictionaryKey) {
         predictionary.useAllDictionaries();
+        _usedKeys = predictionary.getDictionaryKeys();
     } else {
         predictionary.useDictionary(dictionaryKey);
+        if (_usedKeys.indexOf(dictionaryKey) === -1) {
+            _usedKeys.push(dictionaryKey);
+        }
     }
     let suggestions = predictionary.predict(input, {maxPredicitons: registeredPredictElements.length});
     for (let i = 0; i < registeredPredictElements.length; i++) {
         $(`#${registeredPredictElements[i].id} .text-container span`).text(suggestions[i] ? suggestions[i] : '');
     }
     fontUtil.adaptFontSize($('.item[data-type="ELEMENT_TYPE_PREDICTION"]'));
+};
+
+predictionService.learnFromInput = function (input) {
+    if (!input || !input.trim()) {
+        return;
+    }
+    initIfUnititialized();
+    _unsavedChanges = predictionary.learnFromInput(input) || _unsavedChanges;
 };
 
 predictionService.initWithElements = function (elements) {
@@ -83,9 +96,8 @@ function saveDictionaries() {
     if (!_unsavedChanges) {
         return;
     }
-
     _unsavedChanges = false;
-    predictionary.getDictionaryKeys().forEach(key => {
+    _usedKeys.forEach(key => {
         let dbDict = _dbDictObjects.filter(el => el.dictionaryKey === key)[0] || new Dictionary({dictionaryKey: key});
         dbDict.data = predictionary.dictionaryToJSON(key);
         dataService.saveDictionary(dbDict);
