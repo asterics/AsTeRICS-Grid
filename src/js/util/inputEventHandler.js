0b487459125149ce76a47a5cddc221c5ce1e1fe2
Benjamin Klaus <klaus@technikum-wien.at>
2019-06-14 16:09:52 +0200

    refactored inputEventHandler, changed it to service-type, using callback handlers instead of promises

--- src/js/util/inputEventHandler.js
+++ src/js/util/inputEventHandler.js
@@ -1,109 +1,98 @@
-function InputEventHandler(touchElementId) {
-    var thiz = this;
-    
-    // options
-    var touchMoveLength = 100;
-    var mouseBorderThreshold = 5;
-    
-    // internal
-    var _touchMoveBeginPos = null;
-    var _promiseMouseUpperBorder = null;
-    var _promiseMouseUpperBorderResolve = null;
-    var _promiseSwipeUp = null;
-    var _promiseSwipeUpResolve = null;
-    var _promiseSwipeDown = null;
-    var _promiseSwipeDownResolve = null;
-    var _touchElement = document.getElementById(touchElementId) || document;
+import {util} from "./util";
 
-    function initPromises() {
-        _promiseMouseUpperBorder = new Promise(function (resolve) {
-            _promiseMouseUpperBorderResolve = resolve;
-        });
-        _promiseSwipeDown = new Promise(function (resolve) {
-            _promiseSwipeDownResolve = resolve;
-        });
-        _promiseSwipeUp = new Promise(function (resolve) {
-            _promiseSwipeUpResolve = resolve;
-        });
+let inputEventHandler = {};
+
+// options
+let touchMoveLength = 100;
+let mouseBorderThreshold = 5;
+
+// internal
+let _touchMoveBeginPosX = null;
+let _touchMoveBeginPosY = null;
+let mouseBorderHandlers = [];
+let swipeUpHandlers = [];
+let swipeDownHandlers = [];
+let swipeLeftHandles = [];
+let swipeRightHandles = [];
+let _touchElement = document;
+
+inputEventHandler.startListening = function () {
+    document.addEventListener('mousemove', mouseMoveListener);
+    _touchElement.addEventListener('touchmove', touchMoveListener);
+    _touchElement.addEventListener('touchend', touchEndListener);
+};
+
+inputEventHandler.stopListening = function () {
+    document.removeEventListener('mousemove', mouseMoveListener);
+    _touchElement.removeEventListener('touchmove', touchMoveListener);
+    _touchElement.removeEventListener('touchend', touchEndListener);
+};
+
+inputEventHandler.onMouseUpperOrLeftBorder = function (fn) {
+    return registerHandler(fn, mouseBorderHandlers);
+};
+
+inputEventHandler.onSwipedDown = function (fn) {
+    return registerHandler(fn, swipeDownHandlers);
+};
+
+inputEventHandler.onSwipedUp = function (fn) {
+    return registerHandler(fn, swipeUpHandlers);
+};
+
+inputEventHandler.onSwipedRight = function (fn) {
+    return registerHandler(fn, swipeRightHandles);
+};
+
+inputEventHandler.onSwipedLeft = function (fn) {
+    return registerHandler(fn, swipeLeftHandles);
+};
+
+function mouseMoveListener(event) {
+    if (event.clientY < mouseBorderThreshold || event.clientX < mouseBorderThreshold) {
+        callHandlers(mouseBorderHandlers);
     }
-
-    function resolveMouseUpperBorder() {
-        if(_promiseMouseUpperBorderResolve) {
-            _promiseMouseUpperBorderResolve();
-        }
-        _promiseMouseUpperBorder = new Promise(function (resolve) {
-            _promiseMouseUpperBorderResolve = resolve;
-        });
-    }
-
-    function resolveSwipeDown() {
-        if(_promiseSwipeDownResolve) {
-            _promiseSwipeDownResolve();
-        }
-        _promiseSwipeDown = new Promise(function (resolve) {
-            _promiseSwipeDownResolve = resolve;
-        });
-    }
-
-    function resolveSwipeUp() {
-        if(_promiseSwipeUpResolve) {
-            _promiseSwipeUpResolve();
-        }
-        _promiseSwipeUp = new Promise(function (resolve) {
-            _promiseSwipeUpResolve = resolve;
-        });
-    }
-
-    function mouseMoveListener(event) {
-        if(event.clientY < mouseBorderThreshold) {
-            resolveMouseUpperBorder();
-        }
-    }
-
-    function touchMoveListener(event) {
-        if(!_touchMoveBeginPos) {
-            _touchMoveBeginPos = event.touches[0].clientY;
-        } else if (event.touches[0].clientY > _touchMoveBeginPos + touchMoveLength) {
-            _touchMoveBeginPos = null;
-            resolveSwipeDown();
-        } else if (event.touches[0].clientY < _touchMoveBeginPos - touchMoveLength) {
-            _touchMoveBeginPos = null;
-            resolveSwipeUp();
-        }
-    }
-
-    function touchEndListener() {
-        _touchMoveBeginPos = null;
-    }
-
-    thiz.startListening = function () {
-        document.addEventListener('mousemove', mouseMoveListener);
-        _touchElement.addEventListener('touchmove', touchMoveListener);
-        _touchElement.addEventListener('touchend', touchEndListener);
-    };
-
-    thiz.stopListening = function () {
-        initPromises();
-        document.removeEventListener('mousemove', mouseMoveListener);
-        _touchElement.removeEventListener('touchmove', touchMoveListener);
-        _touchElement.removeEventListener('touchend', touchEndListener);
-    };
-
-    thiz.waitMouseUpperBorder = function () {
-        return _promiseMouseUpperBorder;
-    };
-
-    thiz.waitSwipedDown = function () {
-        return _promiseSwipeDown;
-    };
-
-    thiz.waitSwipedUp = function () {
-        return _promiseSwipeUp;
-    };
-
-    //start
-    initPromises();
-    thiz.startListening();
 }
 
-export {InputEventHandler};
+function touchMoveListener(event) {
+    if (!_touchMoveBeginPosY || !_touchMoveBeginPosX) {
+        _touchMoveBeginPosY = event.touches[0].clientY;
+        _touchMoveBeginPosX = event.touches[0].clientX;
+    } else if (event.touches[0].clientY > _touchMoveBeginPosY + touchMoveLength) {
+        log.warn('down')
+        _touchMoveBeginPosY = null;
+        callHandlers(swipeDownHandlers);
+    } else if (event.touches[0].clientY < _touchMoveBeginPosY - touchMoveLength) {
+        log.warn('up')
+        _touchMoveBeginPosY = null;
+        callHandlers(swipeUpHandlers);
+    } else if (event.touches[0].clientX > _touchMoveBeginPosX + touchMoveLength) {
+        log.warn('right')
+        _touchMoveBeginPosX = null;
+        callHandlers(swipeRightHandles);
+    } else if (event.touches[0].clientX < _touchMoveBeginPosX - touchMoveLength) {
+        log.warn('left')
+        _touchMoveBeginPosX = null;
+        callHandlers(swipeLeftHandles);
+    }
+}
+
+function touchEndListener() {
+    _touchMoveBeginPosX = null;
+    _touchMoveBeginPosY = null;
+}
+
+function registerHandler(fn, array) {
+    if (fn) {
+        array.push(fn);
+    }
+    return inputEventHandler;
+}
+
+function callHandlers(array) {
+    array.forEach(handler => {
+        util.throttle(handler);
+    });
+}
+
+export {inputEventHandler};
