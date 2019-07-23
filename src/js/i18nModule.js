a79da993c80e0e375fb0f79a57752a1a2e5aa948
Benjamin Klaus <klaus@technikum-wien.at>
2019-07-23 12:38:25 +0200

    added possibility to switch language in console by calling "changeLanguage"

--- src/js/i18nModule.js
+++ src/js/i18nModule.js
@@ -1,19 +1,31 @@
+import {localStorageService} from "./service/data/localStorageService";
+
 let I18nModule = {};
+let CUSTOM_LANGUAGE_KEY = 'CUSTOM_LANGUAGE_KEY';
+let customLanguage = localStorageService.get(CUSTOM_LANGUAGE_KEY);
 
 I18nModule.init = function () {
-    window.domI18n({
+    let i18n = window.domI18n({
         selector: '[data-i18n]',
         separator: ' // ',
         languages: ['en', 'de']
     });
+    if (customLanguage) {
+        i18n.changeLanguage(customLanguage)
+    }
 };
 
 I18nModule.getBrowserLang = function () {
-    return navigator.language.substring(0, 2);
+    return customLanguage || navigator.language.substring(0, 2);
 };
 
 I18nModule.isBrowserLangDE = function () {
     return I18nModule.getBrowserLang() === 'de';
+};
+
+window.setLanguage = function (lang) {
+    customLanguage = lang;
+    localStorageService.save(CUSTOM_LANGUAGE_KEY, customLanguage);
 };
 
 export {I18nModule};
