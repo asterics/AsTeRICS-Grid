fc89757d876f28c61bf8affa57ff8f4d42f0223c
Benjamin Klaus <klaus@technikum-wien.at>
2019-08-02 11:39:37 +0200

    added scripts to start and stop live logging dashboard with goaccess, https://github.com/allinurl/goaccess

---
+++ docs/logstart.sh
@@ -0,0 +1,2 @@
+goaccess /home/superlogin/AsTeRICS-Grid/superlogin/access.log -o /var/www/html/report.html --log-format=COMBINED --ws-url=couchdb.asterics-foundation.org --port=7890 --real-time-html --ssl-cert=/opt/couchdb/ssl/asterics-foundation.org_ssl_certificate_combined.cer --ssl-key=/opt/couchdb/ssl/asterics-foundation.org_private_key.key &
+
