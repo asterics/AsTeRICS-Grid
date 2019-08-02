7ef5023100898aada14d733a453fcbb089783c99
Benjamin Klaus <klaus@technikum-wien.at>
2019-08-02 12:19:48 +0200

    added geolocation configuration

--- docs/logstart.sh
+++ docs/logstart.sh
@@ -1,2 +1,2 @@
-goaccess /home/superlogin/AsTeRICS-Grid/superlogin/access.log -o /var/www/html/report.html --log-format=COMBINED --ws-url=couchdb.asterics-foundation.org --port=7890 --real-time-html --ssl-cert=/opt/couchdb/ssl/asterics-foundation.org_ssl_certificate_combined.cer --ssl-key=/opt/couchdb/ssl/asterics-foundation.org_private_key.key &
+goaccess /home/superlogin/AsTeRICS-Grid/superlogin/access.log -o /var/www/html/report.html --log-format=COMBINED --ws-url=couchdb.asterics-foundation.org --port=7890 --real-time-html --ssl-cert=/opt/couchdb/ssl/asterics-foundation.org_ssl_certificate_combined.cer --ssl-key=/opt/couchdb/ssl/asterics-foundation.org_private_key.key --geoip-database=/home/superlogin/geolite/GeoLite2-City.mmdb &
 
