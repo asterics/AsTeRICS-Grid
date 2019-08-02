e5f5f5be70226b4714a7f9914064efb1708735ee
Benjamin Klaus <klaus@technikum-wien.at>
2019-08-02 14:01:47 +0200

    stopping before start

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,3 @@
+/bin/sh /home/superlogin/AsTeRICS-Grid/scripts/stop.sh
 /bin/sh -c "sudo -u superlogin npm --prefix /home/superlogin/AsTeRICS-Grid/ run start-superlogin-prod" &
 /bin/sh -c "sudo -u superlogin /bin/sh /home/superlogin/AsTeRICS-Grid/scripts/goaccess_start.sh" &
