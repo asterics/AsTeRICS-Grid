a2c87864b04fcd0698e12495ab1aae935a234937
Benjamin Klaus <klaus@technikum-wien.at>
2019-08-02 13:40:49 +0200

    moved and renamed logstart and logstop.sh to /scripts, added start script that starts superlogin and goaccess log visualization

---
+++ scripts/start.sh
@@ -0,0 +1,2 @@
+/bin/sh -c sudo -u superlogin npm --prefix /home/superlogin/AsTeRICS-Grid/ run start-superlogin-prod &
+/bin/sh -c sudo -u superlogin sh /home/superlogin/AsTeRICS-Grid/scripts/goaccess_start.sh &
