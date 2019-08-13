868938ce32b4f6e0bcc76fce31463321a6b5b6e6
Benjamin Klaus <klaus@technikum-wien.at>
2019-08-13 11:48:27 +0200

    added method urlToBase64() to retrieve image by URL, added SVG support. do not convert images that are SVG (bug in Firefox), improved error handling

--- src/js/util/imageUtil.js
+++ src/js/util/imageUtil.js
@@ -11,7 +11,9 @@ imageUtil.getBase64FromImg = function (img, maxWidth, quality) {
     maxWidth = maxWidth || 150;
     let mimeType = img.src.indexOf('data:') === 0 ? img.src.substring(5, img.src.indexOf(';')) : null;
     mimeType = mimeType || (img.src.indexOf('.png') > -1 ? 'image/png' : null);
+    mimeType = mimeType || (img.src.indexOf('.svg') > -1 ? 'image/svg+xml' : null);
     mimeType = mimeType || 'image/jpeg';
+
     var canvas = document.createElement("canvas");
     var factor = 1;
     if (img.width > maxWidth) {
@@ -21,7 +23,12 @@ imageUtil.getBase64FromImg = function (img, maxWidth, quality) {
     canvas.height = img.height * factor;
     var ctx = canvas.getContext("2d");
     ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-    return canvas.toDataURL(mimeType, quality);
+    try {
+        let data = canvas.toDataURL(mimeType, quality);
+        return data;
+    } catch (e) {
+        throw "image converting failed!"
+    }
 };
 
 /**
@@ -44,28 +51,52 @@ imageUtil.getBase64FromInput = function (input) {
 };
 
 imageUtil.convertBase64 = function (originalBase64, maxWidth, quality) {
-    return new Promise(resolve => {
-        if(!originalBase64) {
-            resolve(null);
-            return;
+    return new Promise((resolve, reject) => {
+        if (!originalBase64) {
+            return resolve(null);
+        }
+        if(originalBase64.substring(5, originalBase64.indexOf(';')) === 'image/svg+xml') {
+            return resolve(originalBase64);
         }
         maxWidth = maxWidth || 150;
         var img = document.createElement('img');
         img.onload = function () {
-            resolve(imageUtil.getBase64FromImg(img, maxWidth, quality));
+            try {
+                resolve(imageUtil.getBase64FromImg(img, maxWidth, quality));
+            } catch (e) {
+                resolve(null);
+            }
         };
         img.src = originalBase64;
     })
 };
 
 imageUtil.urlToBase64 = function (url) {
-    let img = new Image();
-    img.crossOrigin = "anonymous";
-    return new Promise(resolve => {
-        img.onload = function () {
-            resolve(imageUtil.getBase64FromImg(img));
-        };
-        img.src = url;
+    return new Promise((resolve, reject) => {
+        if (url.indexOf('.svg') > -1) {
+            $.get(url, null, function (svgDocument) {
+                //needed because Firefox doesn't correctly handle SVG with size = 0, see https://bugzilla.mozilla.org/show_bug.cgi?id=700533
+                try {
+                    svgDocument.documentElement.width.baseVal.value = svgDocument.documentElement.width.baseVal.value || 500;
+                    svgDocument.documentElement.height.baseVal.value = svgDocument.documentElement.height.baseVal.value || 500;
+                    let base64EncodedSVG = btoa(new XMLSerializer().serializeToString(svgDocument));
+                    resolve('data:image/svg+xml;base64,' + base64EncodedSVG);
+                } catch (e) {
+                    resolve(null);
+                }
+            });
+        } else {
+            let img = new Image();
+            img.crossOrigin = "anonymous";
+            img.onload = function () {
+                try {
+                    resolve(imageUtil.getBase64FromImg(img, 1000));
+                } catch (e) {
+                    resolve(null);
+                }
+            };
+            img.src = url;
+        }
     });
 };
 
