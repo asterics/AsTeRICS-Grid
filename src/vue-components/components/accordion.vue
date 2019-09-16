d05bd10ffc6395d868177ecc82b37eac0c88a5b2
Benjamin Klaus <klaus@technikum-wien.at>
2019-09-16 15:24:39 +0200

    extracted general "accordion component" out of "comparison component"

---
+++ src/vue-components/components/accordion.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <button @click="() => {open = !open}" class="btn-accordion" style="margin-bottom: 0">
+            <i class="fas fa-chevron-down" v-show="!open"></i>
+            <i class="fas fa-chevron-up" v-show="open"></i>
+            <span>{{label | translate}}</span>
+        </button>
+        <div v-show="open" style="background-color: whitesmoke;">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    import {i18nService} from "../../js/service/i18nService";
+
+    export default {
+        props: ["accLabel"],
+        data() {
+            return {
+                open: false,
+                label: ''
+            }
+        },
+        methods: {
+        },
+        mounted() {
+            i18nService.initDomI18n();
+            this.label = this.accLabel;
+        }
+    }
+</script>
+
+<style scoped>
+    .btn-accordion {
+        background-color: white;
+        border-style: solid;
+        border-color: gray;
+        border-width: 1px;
+        text-align: left;
+        border-left: none;
+        border-right: none;
+        width: 100%;
+    }
+    .btn-accordion:hover, .btn-accordion:focus {
+        outline: 2px solid lightblue;
+    }
+    .btn-accordion:hover span {
+        color: #2d7bb4;
+    }
+</style>
--- src/vue-components/components/comparisonComponent.vue
+++ src/vue-components/components/comparisonComponent.vue
@@ -1,41 +1,36 @@
 <template>
     <div>
-        <div class="row" style="margin-bottom: 0">
-            <button @click="() => {showInfo = !showInfo}" class="six columns offset-by-two btn-accordion" style="margin-bottom: 0">
-                <i class="fas fa-chevron-down" v-show="!showInfo"></i>
-                <i class="fas fa-chevron-up" v-show="showInfo"></i>
-                <span data-i18n="">Information about online/offline users // Informationen über Online-/Offline-User</span>
-            </button>
-        </div>
-        <div class="row">
-            <div class="six columns offset-by-two" v-show="showInfo" style="background-color: whitesmoke;">
-                <div>
-                    <h3 data-i18n="">Offline user // Offline-User</h3>
-                    <ul class="fa-ul">
-                        <li><span class="fa-li"><i class="fas fa-check"></i></span><span data-i18n="">all grids are saved offline // alle Grids werden offline gespeichert</span></li>
-                        <li><span class="fa-li"><i class="fas fa-check"></i></span><span data-i18n="">all functions available // voller Funktionsumfang</span></li>
-                        <li><span class="fa-li"><i class="fas fa-check"></i></span><span data-i18n="">ideal for using on a single device // optimal für Nutzung auf einem einzelnen Gerät</span></li>
-                    </ul>
-                </div>
-                <div>
-                    <h3 data-i18n="">Online user // Online-User</h3>
-                    <ul class="fa-ul">
-                        <li><span class="fa-li"><i class="fas fa-check"></i></span><span data-i18n="">all grids are saved offline and online // alle Grids werden offline und online gespeichert</span></li>
-                        <li><span class="fa-li"><i class="fas fa-check"></i></span><span data-i18n="">automatic synchronization across multiple devices (e.g. PC, tablet, smartphone) // automatische Synchronisierung zwischen verschiedenen Geräten (z.B. PC, Tablet, Smartphone)</span></li>
-                        <li><span class="fa-li"><i class="fas fa-check"></i></span><span data-i18n="">all data is end-to-end encrypted and only you are able to access it // alle Daten werden verschlüsselt und es kann niemand außer Sie darauf zugreifen</span></li>
-                        <li><span class="fa-li"><i class="fas fa-check"></i></span><span data-i18n="">for registration only a username and a password is needed // für die Registrierung wird nur ein Username und ein Passwort benötigt</span></li>
-                    </ul>
-                </div>
+        <accordion acc-label="HEADER_COMPARE_ONLINE_OFFLINE" class="six columns offset-by-two">
+            <div>
+                <h3 data-i18n="">Offline user // Offline-User</h3>
+                <ul class="fa-ul">
+                    <li><span class="fa-li"><i class="fas fa-check"></i></span><span data-i18n="">all grids are saved offline // alle Grids werden offline gespeichert</span></li>
+                    <li><span class="fa-li"><i class="fas fa-check"></i></span><span data-i18n="">all functions available // voller Funktionsumfang</span></li>
+                    <li><span class="fa-li"><i class="fas fa-check"></i></span><span data-i18n="">ideal for using on a single device // optimal für Nutzung auf einem einzelnen Gerät</span></li>
+                </ul>
             </div>
-        </div>
+            <div>
+                <h3 data-i18n="">Online user // Online-User</h3>
+                <ul class="fa-ul">
+                    <li><span class="fa-li"><i class="fas fa-check"></i></span><span data-i18n="">all grids are saved offline and online // alle Grids werden offline und online gespeichert</span></li>
+                    <li><span class="fa-li"><i class="fas fa-check"></i></span><span data-i18n="">automatic synchronization across multiple devices (e.g. PC, tablet, smartphone) // automatische Synchronisierung zwischen verschiedenen Geräten (z.B. PC, Tablet, Smartphone)</span></li>
+                    <li><span class="fa-li"><i class="fas fa-check"></i></span><span data-i18n="">all data is end-to-end encrypted and only you are able to access it // alle Daten werden verschlüsselt und es kann niemand außer Sie darauf zugreifen</span></li>
+                    <li><span class="fa-li"><i class="fas fa-check"></i></span><span data-i18n="">for registration only a username and a password is needed // für die Registrierung wird nur ein Username und ein Passwort benötigt</span></li>
+                </ul>
+            </div>
+        </accordion>
     </div>
 </template>
 
 <script>
     import {i18nService} from "../../js/service/i18nService";
+    import Accordion from "./accordion.vue"
 
     export default {
         props: [],
+        components: {
+            Accordion
+        },
         data() {
             return {
                 showInfo: false
@@ -51,7 +46,9 @@
 
 <style scoped>
     h3 {
-        margin-bottom: 0.5em;
+        margin: 0;
+        padding-bottom: 0.5em;
+        padding-top: 1em;
     }
     li {
         margin-bottom: 0;
@@ -64,21 +61,5 @@
     .fa-check {
         color: green;
         margin-right: 1em;
-    }
-
-    .btn-accordion {
-        background-color: white;
-        border-style: solid;
-        border-color: gray;
-        border-width: 1px;
-        text-align: left;
-        border-left: none;
-        border-right: none;
-    }
-    .btn-accordion:hover, .btn-accordion:focus {
-        outline: 2px solid lightblue;
-    }
-    .btn-accordion:hover span {
-        color: #2d7bb4;
     }
 </style>
