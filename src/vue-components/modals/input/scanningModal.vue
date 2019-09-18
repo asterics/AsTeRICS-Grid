77bf6d42e74abbe66c4b5e83c3148217948783b8
Benjamin Klaus <klaus@technikum-wien.at>
2019-09-18 12:21:24 +0200

    added modal for defining scanning input method

---
+++ src/vue-components/modals/input/scanningModal.vue
@@ -0,0 +1,170 @@
+<template>
+    <div class="modal">
+        <div class="modal-mask">
+            <div class="modal-wrapper">
+                <div class="modal-container" @keyup.27="cancel()" @keyup.enter="save()">
+                    <a class="inline close-button" href="javascript:void(0);" @click="cancel()"><i class="fas fa-times"/></a>
+                    <a class="close-button" href="javascript:;" @click="openHelp()"><i class="fas fa-question-circle"></i></a>
+                    <div class="modal-header">
+                        <h1 name="header">
+                            Scanning
+                        </h1>
+                    </div>
+
+                    <div class="modal-body" v-if="inputConfig">
+                        <div class="row">
+                            <span data-i18n="">Scanning input method: 1-2 input channels // Eingabemethode Scanning: 1-2 Eingabekanäle</span>
+                            <a aria-label="Help" href="javascript:;" @click="openHelp()"><i class="fas blue fa-question-circle"></i></a>
+                        </div>
+                        <div class="row">
+                            <div class="twelve columns">
+                                <div class="row" >
+                                    <div class="twelve columns">
+                                        <input v-focus type="checkbox" id="enableScanning" v-model="inputConfig.scanEnabled"/>
+                                        <label class="inline" for="enableScanning" data-i18n>Enable Scanning // Scanning aktivieren</label>
+                                    </div>
+                                </div>
+                                <div v-show="inputConfig.scanEnabled">
+                                    <accordion acc-label="Input // Eingabe" acc-open="true" acc-label-type="h2" acc-background-color="white" class="row">
+                                        <input-event-list v-model="inputConfig.scanInputs" :input-labels="[InputConfig.SELECT, InputConfig.NEXT]" :error-inputs="errorInputs" @input="inputChanged"></input-event-list>
+                                    </accordion>
+                                    <accordion acc-label="ADVANCED_SETTINGS" acc-label-type="h2" acc-background-color="white">
+                                        <div class="row" style="margin-top: 0">
+                                            <div class="six columns">
+                                                <input type="checkbox" id="chkVerticalScanning" v-model="inputConfig.scanVertical"/>
+                                                <label for="chkVerticalScanning" data-i18n>Vertical scanning // Scanning vertikal</label>
+                                            </div>
+                                            <div class="six columns">
+                                                <input type="checkbox" id="chkBinaryScanning" v-model="inputConfig.scanBinary"/>
+                                                <label for="chkBinaryScanning" data-i18n>Binary scanning // Scanning binär</label>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="six columns slidergroup">
+                                                <label for="inScanTime" data-i18n>Scanning Time (ms) // Scanning Zeit (ms)</label>
+                                                <input type="range" id="inScanTime" v-model.number="inputConfig.scanTimeoutMs" min="100" max="3000" step="100"/>
+                                                <input type="number" v-model.number="inputConfig.scanTimeoutMs" min="100" max="3000" step="100"/>
+                                            </div>
+                                            <div class="six columns slidergroup">
+                                                <label for="inFirstElement" data-i18n>Time factor first element // Zeit-Faktor erstes Element</label>
+                                                <input type="range" id="inFirstElement" v-model.number="inputConfig.scanTimeoutFirstElementFactor" min="1" max="5" step="0.1"/>
+                                                <input type="number" v-model.number="inputConfig.scanTimeoutFirstElementFactor" min="1" max="5" step="0.5" />
+                                            </div>
+                                        </div>
+                                    </accordion>
+                                    <accordion acc-label="TEST_CONFIGURATION" acc-label-type="h2" acc-background-color="white">
+                                    </accordion>
+
+                                    <div class="warn" v-show="error">
+                                        <i class="fas fa-exclamation-triangle"></i> {{error}}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="modal-footer">
+                        <div class="button-container">
+                            <button @click="cancel()">
+                                <i class="fas fa-times"/> <span data-i18n>Cancel // Abbrechen</span>
+                            </button>
+                            <button @click="save()">
+                                 <i class="fas fa-check"/> <span>OK</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import {dataService} from '../../../js/service/data/dataService'
+    import {helpService} from "../../../js/service/helpService";
+    import {i18nService} from "../../../js/service/i18nService";
+    import Accordion from "../../components/accordion.vue"
+    import InputEventList from "../../components/inputEventList.vue"
+    import './../../../css/modal.css';
+    import {InputConfig} from "../../../js/model/InputConfig";
+
+    export default {
+        props: [],
+        components: {Accordion, InputEventList},
+        data: function () {
+            return {
+                inputConfig: null,
+                metadata: null,
+                isRecordingKey: false,
+                isRecordingARE: false,
+                lastRecordTime: 0,
+                areConnectionError: false,
+                InputConfig: InputConfig,
+                error: '',
+                errorInputs: []
+            }
+        },
+        methods: {
+            save() {
+                if (!this.validateInputs()) {
+                    return;
+                }
+                this.metadata.inputConfig = this.inputConfig;
+                dataService.saveMetadata(this.metadata).then(() => {
+                    this.$emit('close');
+                });
+            },
+            cancel() {
+                this.$emit('close');
+            },
+            openHelp() {
+                helpService.openHelp();
+            },
+            validateInputs() {
+                this.errorInputs = [];
+                this.error = "";
+                if (!this.inputConfig.scanEnabled) {
+                    return true;
+                }
+                if (this.inputConfig.scanInputs.filter(input => input.label === InputConfig.SELECT).length === 0) {
+                    this.errorInputs.push(InputConfig.SELECT);
+                }
+                if (this.inputConfig.scanInputs.filter(input => input.label === InputConfig.NEXT).length === 0 && !this.inputConfig.scanAuto) {
+                    this.errorInputs.push(InputConfig.NEXT);
+                }
+
+                if (this.errorInputs.length > 0) {
+                    this.error = i18nService.translate('Please specify input modalities // Bitte Eingabemodalitäten definieren');
+                    return false;
+                }
+                return true;
+            },
+            inputChanged() {
+                if (this.error) {
+                    this.validateInputs();
+                }
+            }
+        },
+        mounted () {
+            let thiz = this;
+            log.debug('opened modal: ' + thiz.gridId);
+            dataService.getMetadata().then(metadata => {
+                thiz.metadata = JSON.parse(JSON.stringify(metadata));
+                thiz.inputConfig = JSON.parse(JSON.stringify(metadata.inputConfig));
+            });
+            helpService.setHelpLocation('04_input_options', '#input-options');
+        },
+        updated() {
+            i18nService.initDomI18n();
+        },
+        beforeDestroy() {
+            helpService.revertToLastLocation();
+        }
+    }
+</script>
+
+<style scoped>
+    .warn {
+        margin-top: 2em;
+    }
+</style>
