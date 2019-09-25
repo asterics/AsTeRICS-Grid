2c7ddb686cea51cfc629a3575845d9ad2abb681a
Benjamin Klaus <klaus@technikum-wien.at>
2019-09-25 11:28:04 +0200

    fixed "NPE" if no result of getObject()

--- src/js/service/data/databaseService.js
+++ src/js/service/data/databaseService.js
@@ -41,8 +41,8 @@ databaseService.getObject = function (objectType, id, onlyShortVersion) {
                     onlyShortVersion: onlyShortVersion
                 };
                 let filteredData = filterService.convertDatabaseToLiveObjects(result, options);
-                let modelVersion = filteredData.modelVersion || filteredData[0].modelVersion;
-                if (_lastDataModelVersion !== modelVersion) {
+                let modelVersion = filteredData ? (filteredData.modelVersion || filteredData[0].modelVersion) : null;
+                if (modelVersion && _lastDataModelVersion !== modelVersion) {
                     _lastDataModelVersion = modelVersion;
                     localStorageService.setUserModelVersion(pouchDbService.getOpenedDatabaseName(), modelVersion);
                 }
