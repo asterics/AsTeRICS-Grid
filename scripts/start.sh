22dc9bade19136518ed2a0dfe38068cdc46875a1
Benjamin Klaus <klaus@technikum-wien.at>
2020-02-10 11:13:47 +0100

    removed sudo from start script -> should be started by superlogin user

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 /bin/sh /home/superlogin/AsTeRICS-Grid/scripts/stop.sh
-/bin/sh -c "sudo -u superlogin npm --prefix /home/superlogin/AsTeRICS-Grid/ run start-superlogin-prod" &
-/bin/sh -c "sudo -u superlogin /bin/sh /home/superlogin/AsTeRICS-Grid/scripts/goaccess_start.sh" &
+/bin/sh -c "npm --prefix /home/superlogin/AsTeRICS-Grid/ run start-superlogin-prod" &
+/bin/sh -c "/bin/sh /home/superlogin/AsTeRICS-Grid/scripts/goaccess_start.sh" &
