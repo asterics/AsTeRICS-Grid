04b208af857b71ed902ad9fb1b6f014e6c809644
Benjamin Klaus <klaus@technikum-wien.at>
2020-02-26 10:40:28 +0100

    added fileUtil for reading zipped (.obz) files, dynamically loading jszip library

---
+++ src/js/util/fileUtil.js
@@ -0,0 +1,33 @@
+let fileUtil = {};
+
+/**
+ * reads the contents of a .zip file
+ * @param file the zip file to read
+ * @param parseJSON if true the file content is parsed as JSON
+ * @return a Promise resolving to a map {filename => String file content}
+ */
+fileUtil.readZip = function (file, parseJSON) {
+    let returnMap = {};
+    return new Promise(resolve => {
+        import(/* webpackChunkName: "JSZip" */ 'jszip').then(JSZip => {
+            JSZip.loadAsync(file).then(zip => {
+                let promises = [];
+                Object.keys(zip.files).forEach(filename => {
+                    let file = zip.files[filename];
+                    promises.push(file.async('text').then(content => {
+                        try {
+                            returnMap[filename] = parseJSON ? JSON.parse(content) : content;
+                        } catch (e) {
+                            returnMap[filename] = content;
+                        }
+                    }));
+                });
+                Promise.all(promises).then(() => {
+                    resolve(returnMap);
+                });
+            });
+        })
+    });
+};
+
+export {fileUtil};
