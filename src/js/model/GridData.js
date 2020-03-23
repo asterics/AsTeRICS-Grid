f9da3f0912ecd2a3672e0a30bad412a253dc87f6
Benjamin Klaus <klaus@technikum-wien.at>
2020-03-23 12:08:56 +0100

    added methods getWidth() and getHeight(), added static methods getOffset() and generateGlobalGrid() - global grid may should be moved to static external file

--- src/js/model/GridData.js
+++ src/js/model/GridData.js
@@ -5,6 +5,8 @@ import {GridActionARE} from "./GridActionARE";
 import {constants} from "../util/constants";
 import {Model} from "../externals/objectmodel";
 import {Webradio} from "./Webradio";
+import {i18nService} from "../service/i18nService";
+import {GridImage} from "./GridImage";
 
 class GridData extends Model({
     id: String,
@@ -29,6 +31,14 @@ class GridData extends Model({
 
     hasSetPositions() {
         return this.gridElements.every(elm => elm.hasSetPosition());
+    }
+
+    getWidth() {
+        return Math.max.apply(null, this.gridElements.map(el => el.x + el.width));
+    }
+
+    getHeight() {
+        return Math.max.apply(null, this.gridElements.map(el => el.y + el.height));
     }
 
     /**
@@ -221,6 +231,107 @@ class GridData extends Model({
             if (a.y !== b.y) return a.y - b.y;
             return a.x - b.x;
         });
+    }
+
+    /**
+     * generates a global grid with elements "home", "back", input field, "backspace" and "clear"
+     * @param width the with to generate
+     * @return {GridData}
+     */
+    static generateGlobalGrid(width) {
+        width = width || 0;
+        let elementHome = new GridElement({
+            width: 1,
+            height: 1,
+            x: 0,
+            y: 0,
+            label: i18nService.translate('Home // Start'),
+            image: new GridImage({
+                data: 'data:image/svg+xml;base64,' + 'PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA1NzYgNTEyIj48cGF0aCBkPSJNMjgwLjM3IDE0OC4yNkw5NiAzMDAuMTFWNDY0YTE2IDE2IDAgMCAwIDE2IDE2bDExMi4wNi0uMjlhMTYgMTYgMCAwIDAgMTUuOTItMTZWMzY4YTE2IDE2IDAgMCAxIDE2LTE2aDY0YTE2IDE2IDAgMCAxIDE2IDE2djk1LjY0YTE2IDE2IDAgMCAwIDE2IDE2LjA1TDQ2NCA0ODBhMTYgMTYgMCAwIDAgMTYtMTZWMzAwTDI5NS42NyAxNDguMjZhMTIuMTkgMTIuMTkgMCAwIDAtMTUuMyAwek01NzEuNiAyNTEuNDdMNDg4IDE4Mi41NlY0NC4wNWExMiAxMiAwIDAgMC0xMi0xMmgtNTZhMTIgMTIgMCAwIDAtMTIgMTJ2NzIuNjFMMzE4LjQ3IDQzYTQ4IDQ4IDAgMCAwLTYxIDBMNC4zNCAyNTEuNDdhMTIgMTIgMCAwIDAtMS42IDE2LjlsMjUuNSAzMUExMiAxMiAwIDAgMCA0NS4xNSAzMDFsMjM1LjIyLTE5My43NGExMi4xOSAxMi4xOSAwIDAgMSAxNS4zIDBMNTMwLjkgMzAxYTEyIDEyIDAgMCAwIDE2LjktMS42bDI1LjUtMzFhMTIgMTIgMCAwIDAtMS43LTE2LjkzeiIvPjwvc3ZnPg'
+            })
+        });
+        let elementBack = new GridElement({
+            width: 1,
+            height: 1,
+            x: 1,
+            y: 0,
+            label: i18nService.translate('Back // Zurück'),
+            image: new GridImage({
+                data: 'data:image/svg+xml;base64,' + 'PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA1MTIgNTEyIj48cGF0aCBkPSJNMjU2IDUwNEMxMTkgNTA0IDggMzkzIDggMjU2UzExOSA4IDI1NiA4czI0OCAxMTEgMjQ4IDI0OC0xMTEgMjQ4LTI0OCAyNDh6bTExNi0yOTJIMjU2di03MC45YzAtMTAuNy0xMy0xNi4xLTIwLjUtOC41TDEyMS4yIDI0Ny41Yy00LjcgNC43LTQuNyAxMi4yIDAgMTYuOWwxMTQuMyAxMTQuOWM3LjYgNy42IDIwLjUgMi4yIDIwLjUtOC41VjMwMGgxMTZjNi42IDAgMTItNS40IDEyLTEydi02NGMwLTYuNi01LjQtMTItMTItMTJ6Ii8+PC9zdmc+'
+            })
+        });
+        let elementCollect = new GridElement({
+            width: Math.max(4, width - 4),
+            height: 1,
+            x: 2,
+            y: 0,
+            type: GridElement.ELEMENT_TYPE_COLLECT
+        });
+        let elementBackspace = new GridElement({
+            width: 1,
+            height: 1,
+            x: 2 + elementCollect.width,
+            y: 0,
+            image: new GridImage({ //fa backspace
+                data: 'data:image/svg+xml;base64,' + 'PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA2NDAgNTEyIj48cGF0aCBkPSJNNTc2IDY0SDIwNS4yNkE2My45NyA2My45NyAwIDAgMCAxNjAgODIuNzVMOS4zNyAyMzMuMzdjLTEyLjUgMTIuNS0xMi41IDMyLjc2IDAgNDUuMjVMMTYwIDQyOS4yNWMxMiAxMiAyOC4yOCAxOC43NSA0NS4yNSAxOC43NUg1NzZjMzUuMzUgMCA2NC0yOC42NSA2NC02NFYxMjhjMC0zNS4zNS0yOC42NS02NC02NC02NHptLTg0LjY5IDI1NC4wNmM2LjI1IDYuMjUgNi4yNSAxNi4zOCAwIDIyLjYzbC0yMi42MiAyMi42MmMtNi4yNSA2LjI1LTE2LjM4IDYuMjUtMjIuNjMgMEwzODQgMzAxLjI1bC02Mi4wNiA2Mi4wNmMtNi4yNSA2LjI1LTE2LjM4IDYuMjUtMjIuNjMgMGwtMjIuNjItMjIuNjJjLTYuMjUtNi4yNS02LjI1LTE2LjM4IDAtMjIuNjNMMzM4Ljc1IDI1NmwtNjIuMDYtNjIuMDZjLTYuMjUtNi4yNS02LjI1LTE2LjM4IDAtMjIuNjNsMjIuNjItMjIuNjJjNi4yNS02LjI1IDE2LjM4LTYuMjUgMjIuNjMgMEwzODQgMjEwLjc1bDYyLjA2LTYyLjA2YzYuMjUtNi4yNSAxNi4zOC02LjI1IDIyLjYzIDBsMjIuNjIgMjIuNjJjNi4yNSA2LjI1IDYuMjUgMTYuMzggMCAyMi42M0w0MjkuMjUgMjU2bDYyLjA2IDYyLjA2eiIvPjwvc3ZnPg'
+            })
+        });
+        let elementClear = new GridElement({
+            width: 1,
+            height: 1,
+            x: 3 + elementCollect.width,
+            y: 0,
+            image: new GridImage({ //fa times-circle
+                data: 'data:image/svg+xml;base64,' + 'PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA1MTIgNTEyIj48cGF0aCBkPSJNMjU2IDhDMTE5IDggOCAxMTkgOCAyNTZzMTExIDI0OCAyNDggMjQ4IDI0OC0xMTEgMjQ4LTI0OFMzOTMgOCAyNTYgOHptMTIxLjYgMzEzLjFjNC43IDQuNyA0LjcgMTIuMyAwIDE3TDMzOCAzNzcuNmMtNC43IDQuNy0xMi4zIDQuNy0xNyAwTDI1NiAzMTJsLTY1LjEgNjUuNmMtNC43IDQuNy0xMi4zIDQuNy0xNyAwTDEzNC40IDMzOGMtNC43LTQuNy00LjctMTIuMyAwLTE3bDY1LjYtNjUtNjUuNi02NS4xYy00LjctNC43LTQuNy0xMi4zIDAtMTdsMzkuNi0zOS42YzQuNy00LjcgMTIuMy00LjcgMTcgMGw2NSA2NS43IDY1LjEtNjUuNmM0LjctNC43IDEyLjMtNC43IDE3IDBsMzkuNiAzOS42YzQuNyA0LjcgNC43IDEyLjMgMCAxN0wzMTIgMjU2bDY1LjYgNjUuMXoiLz48L3N2Zz4'
+            })
+        });
+        let globalGrid = new GridData({
+            label: 'Global Grid',
+            gridElements: [elementHome, elementBack, elementCollect, elementBackspace, elementClear]
+        });
+        return globalGrid;
+    }
+
+    /**
+     * calculates the x/y offset that is needed for another grid to not interfere with the given global grid
+     * @param globalGrid
+     * @return {{x: *, y: *}}
+     */
+    static getOffset(globalGrid) {
+        let elements = JSON.parse(JSON.stringify(globalGrid.gridElements));
+        let results = [];
+        let minValidSum = 100000;
+        for (let x = 0; x < 10; x++) {
+            for (let y = 0; y < 10; y++) {
+                if (x + y >= minValidSum) {
+                    break;
+                }
+                let valid = elements.reduce((total, element) => {
+                    return total && (y >= (element.y + element.height) || x >= (element.x + element.width));
+                }, true);
+                if (valid) {
+                    minValidSum = x + y;
+                }
+                results.push({
+                    x: x,
+                    y: y,
+                    sum: x + y,
+                    valid: valid
+                });
+            }
+        }
+        let result = results.filter(result => minValidSum === result.sum && result.valid)[0];
+        if (result) {
+            return {
+                x: result.x,
+                y: result.y
+            }
+        } else {
+            return {
+                x: globalGrid.getWidth(),
+                y: globalGrid.getHeight()
+            }
+        }
     }
 
     static getModelName() {
