330427f825ea04d0d81108981663f748eb42e925
Benjamin Klaus <klaus@technikum-wien.at>
2020-05-27 14:36:13 +0200

    made rowCount optional to prevent errors if not set (automatically set below)

--- src/js/model/GridData.js
+++ src/js/model/GridData.js
@@ -15,7 +15,7 @@ class GridData extends Model({
     isShortVersion: Boolean, // if true this object represents a non-full short version excluding binary base64 data
     label: [String],
     locale: [String],
-    rowCount: Number,
+    rowCount: [Number],
     minColumnCount: [Number],
     gridElements: Model.Array(GridElement),
     additionalFiles: [Model.Array(AdditionalGridFile)],
