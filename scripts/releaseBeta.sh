9c4503fdfe4bb5893d26646ab3bb6851e262ee96
Benjamin Klaus <klaus@technikum-wien.at>
2020-08-20 16:52:44 +0200

    added reverting of changes in serviceWorker.js

--- scripts/releaseBeta.sh
+++ scripts/releaseBeta.sh
@@ -56,6 +56,7 @@ git push origin HEAD
 git checkout src/js/mainScript.js
 git checkout src/vue-components/views/aboutView.vue
 git checkout src/js/mainScript.js
+git checkout serviceWorker.js
 git checkout src/js/util/constants.js
 echo "creating tag '$tagname'..."
 git tag -a $tagname -m $tagname
