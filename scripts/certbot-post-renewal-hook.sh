3c3dac45df5e87fcbe02d6c1bf89522f79eb8f24
Benjamin Klaus <klaus@technikum-wien.at>
2020-09-02 11:30:14 +0000

    added certbot renewal script

---
+++ scripts/certbot-post-renewal-hook.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+/home/superlogin/AsTeRICS-Grid/scripts/start.sh &>/dev/null &
+disown
+cp -rfL /etc/letsencrypt/live/ /opt/couchdb/letsencrypt
+chown -R couchdb:couchdb /opt/couchdb/letsencrypt/
+service couchdb restart
+/etc/init.d/apache2 restart
