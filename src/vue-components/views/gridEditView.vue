e0b503bf679eb8ab5b191f324ea6e5374ad83844
Benjamin Klaus <klaus@technikum-wien.at>
2020-10-20 10:41:00 +0200

    removed youtube logo for legal reasons

--- src/vue-components/views/gridEditView.vue
+++ src/vue-components/views/gridEditView.vue
@@ -320,7 +320,7 @@
                     },
                     'CONTEXT_NEW_YT_PLAYER': {
                         name: "New YouTube Player // Neuer YouTube Player",
-                        icon: "fab fa-youtube"
+                        icon: "fas fa-play-circle"
                     }
                 }
             }
