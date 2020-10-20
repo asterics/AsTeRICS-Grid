3f0950a8ab137aafc6352768d0bda3abf0c8b3c3
Benjamin Klaus <klaus@technikum-wien.at>
2020-10-20 14:17:18 +0200

    added possibility to pass actionLinkUrl instead of actionLinkFn

--- src/vue-components/components/notificationBar.vue
+++ src/vue-components/components/notificationBar.vue
@@ -4,7 +4,8 @@
         <i v-if="tooltipOptions.faIcon" :class="tooltipOptions.faIcon"></i>
         <div style="padding-left: 0.5em">
             <span v-html="tooltipHTML"></span>
-            <a href="javascript:;" @click="onActionLink" style="display:block; color: #44a8f1">{{actionLink | translate}}</a>
+            <a v-if="tooltipOptions.actionLinkFn" href="javascript:;" @click="onActionLink" style="display:block; color: #44a8f1">{{actionLink | translate}}</a>
+            <a v-if="tooltipOptions.actionLinkUrl" :href="tooltipOptions.actionLinkUrl" target="_blank" style="display:block; color: #44a8f1">{{actionLink | translate}}</a>
         </div>
         <button @click="tooltipHTML = ''" style="position: absolute; top: 0; right: 10px; padding: 0 10px">X</button>
     </div>
