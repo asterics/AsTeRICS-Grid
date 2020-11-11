3673b0e78da3642077af9fe2c1965cfa5e51f2b1
Benjamin Klaus <klaus@technikum-wien.at>
2020-11-11 16:33:48 +0100

    moved firefox fix to own methods, adding it to base64SvgToBase64Png

--- src/js/util/imageUtil.js
+++ src/js/util/imageUtil.js
@@ -77,14 +77,25 @@ imageUtil.convertBase64 = function (originalBase64, maxWidth, quality) {
  * converts a base64 encoded data url SVG image to a PNG image
  * @param originalBase64 data url of svg image
  * @param width target width in pixel of PNG image
+ * @param secondTry used internally to prevent endless recursion
  * @return {Promise<unknown>} resolves to png data url of the image
  */
-imageUtil.base64SvgToBase64Png = async function (originalBase64, width) {
+imageUtil.base64SvgToBase64Png = function (originalBase64, width, secondTry) {
     return new Promise(resolve => {
         let img = document.createElement('img');
         img.onload = function () {
+            if (!secondTry && (img.naturalWidth === 0 || img.naturalHeight === 0)) {
+                let svgDoc = base64ToSvgDocument(originalBase64);
+                let fixedDoc = fixSvgDocumentFF(svgDoc);
+                return imageUtil.base64SvgToBase64Png(svgDocumentToBase64(fixedDoc), width, true).then(result => {
+                    resolve(result);
+                });
+            }
             let canvas = document.createElement("canvas");
+            let ratio = (img.naturalWidth / img.naturalHeight) || 1;
             canvas.width = width;
+            canvas.height = width / ratio;
+            document.body.appendChild(img);
             let ctx = canvas.getContext("2d");
             ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
             try {
@@ -102,15 +113,8 @@ imageUtil.urlToBase64 = function (url) {
     return new Promise((resolve, reject) => {
         if (url.lastIndexOf('.svg') === url.length - 4) {
             $.get(url, null, function (svgDocument) {
-                //needed because Firefox doesn't correctly handle SVG with size = 0, see https://bugzilla.mozilla.org/show_bug.cgi?id=700533
-                try {
-                    svgDocument.documentElement.width.baseVal.value = svgDocument.documentElement.width.baseVal.value || 500;
-                    svgDocument.documentElement.height.baseVal.value = svgDocument.documentElement.height.baseVal.value || 500;
-                    let base64EncodedSVG = btoa(new XMLSerializer().serializeToString(svgDocument));
-                    resolve('data:image/svg+xml;base64,' + base64EncodedSVG);
-                } catch (e) {
-                    resolve(null);
-                }
+                let fixedSvg = fixSvgDocumentFF(svgDocument);
+                resolve(svgDocumentToBase64(fixedSvg));
             }).fail(function () {
                 resolve(null);
             });
@@ -149,5 +153,34 @@ imageUtil.getScreenshot = function (selector) {
 imageUtil.getEmptyImage = function () {
     return "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=";
 };
+
+//needed because Firefox doesn't correctly handle SVG with size = 0, see https://bugzilla.mozilla.org/show_bug.cgi?id=700533
+function fixSvgDocumentFF(svgDocument) {
+    try {
+        let widthInt = parseInt(svgDocument.documentElement.width.baseVal.value) || 500;
+        let heightInt = parseInt(svgDocument.documentElement.height.baseVal.value) || 500;
+        svgDocument.documentElement.width.baseVal.newValueSpecifiedUnits(SVGLength.SVG_LENGTHTYPE_PX, widthInt);
+        svgDocument.documentElement.height.baseVal.newValueSpecifiedUnits(SVGLength.SVG_LENGTHTYPE_PX, heightInt);
+        return svgDocument;
+    } catch (e) {
+        return svgDocument;
+    }
+}
+
+function svgDocumentToBase64(svgDocument) {
+    try {
+        let base64EncodedSVG = btoa(new XMLSerializer().serializeToString(svgDocument));
+        return 'data:image/svg+xml;base64,' + base64EncodedSVG;
+    } catch (e) {
+        return null;
+    }
+}
+
+function base64ToSvgDocument(base64) {
+    let svg = atob(base64.substring(base64.indexOf('base64,') + 7));
+    svg = svg.substring(svg.indexOf('<svg'));
+    let parser = new DOMParser();
+    return parser.parseFromString(svg, "image/svg+xml");
+}
 
 export {imageUtil};
