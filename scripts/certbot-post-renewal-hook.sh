e80fff31362cfd26eece076930d50c2bf7f57f85
Benjamin Klaus <klaus@technikum-wien.at>
2020-12-02 16:00:17 +0100

    updated/fixed post renew hook, removing copied certs before re-copy, otherwise not overwritten

--- scripts/certbot-post-renewal-hook.sh
+++ scripts/certbot-post-renewal-hook.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-/bin/sh /home/superlogin/AsTeRICS-Grid/scripts/start.sh &>/dev/null &
-disown
+rm -rf /opt/couchdb/letsencrypt
 cp -rfL /etc/letsencrypt/live/ /opt/couchdb/letsencrypt
 chown -R couchdb:couchdb /opt/couchdb/letsencrypt/
+/bin/sh /home/superlogin/AsTeRICS-Grid/scripts/start.sh &>/dev/null &
+disown
 service couchdb restart
 /etc/init.d/apache2 restart
