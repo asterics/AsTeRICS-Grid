a060e66e9ca782f4adec2c177ad9439d2fb07848
Benjamin Klaus <klaus@technikum-wien.at>
2021-02-17 16:26:48 +0100

    updating data to prevent re-copy if running backup twice a day

--- scripts/backupDatabasesNozip.sh
+++ scripts/backupDatabasesNozip.sh
@@ -16,8 +16,8 @@ fi
 foldername=$(date +"%Y_%m_%d")_backup_couchdb_asterics_grid
 mkdir -p $1/$foldername
 echo "Copying data from CouchDB..."
-rsync -LKIr backup-reader@1ce28d.online-server.cloud:/opt/couchdb/data $1/$foldername
-rsync -LKIr backup-reader@1ce28d.online-server.cloud:/opt/couchdb/etc $1/$foldername
+rsync -LKIr --update backup-reader@1ce28d.online-server.cloud:/opt/couchdb/data $1/$foldername
+rsync -LKIr --update backup-reader@1ce28d.online-server.cloud:/opt/couchdb/etc $1/$foldername
 
 echo "Copied data. Size: "$(du -sh $1/$foldername)
 echo "Total size of backups: "$(du -sh $1)
