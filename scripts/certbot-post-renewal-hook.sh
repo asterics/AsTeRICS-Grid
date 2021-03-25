16f503873326802da517f97c21b81af7852474e9
Benjamin Klaus <klaus@technikum-wien.at>
2021-03-25 12:20:48 +0100

    added restart of info-tree REST API to post renewal hook

--- scripts/certbot-post-renewal-hook.sh
+++ scripts/certbot-post-renewal-hook.sh
@@ -5,5 +5,8 @@ cp -rfL /etc/letsencrypt/live/ /opt/couchdb/letsencrypt
 chown -R couchdb:couchdb /opt/couchdb/letsencrypt/
 /bin/sh /home/superlogin/AsTeRICS-Grid/scripts/start.sh &>/dev/null &
 disown
+pkill -f "/usr/bin/node /home/superlogin/accessibility-info-tree/rest/restApi.mjs prod"
+/usr/bin/node /home/superlogin/accessibility-info-tree/rest/restApi.mjs prod &
+disown
 service couchdb restart
 /etc/init.d/apache2 restart
