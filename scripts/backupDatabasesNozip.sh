8c489d7cac316b761673bba8d5ed4efdf8304f14
Benjamin Klaus <klaus@technikum-wien.at>
2021-09-28 14:56:37 +0200

    use limited user backup-reader

--- scripts/backupDatabasesNozip.sh
+++ scripts/backupDatabasesNozip.sh
@@ -17,8 +17,8 @@ foldername=$(date +"%Y_%m_%d")_backup_couchdb_asterics_grid
 log_file=$1/last_rsync_log.txt
 mkdir -p $1/$foldername
 echo "Copying data from CouchDB..."
-rsync -LKIrv --update root@1ce28d.online-server.cloud:/opt/couchdb/data $1/$foldername &> $log_file
-rsync -LKIrv --update root@1ce28d.online-server.cloud:/opt/couchdb/etc $1/$foldername &>> $log_file
+rsync -LKIrv --update backup-reader@1ce28d.online-server.cloud:/opt/couchdb/data $1/$foldername &> $log_file
+rsync -LKIrv --update backup-reader@1ce28d.online-server.cloud:/opt/couchdb/etc $1/$foldername &>> $log_file
 
 echo "Copied data. Size: "$(du -sh $1/$foldername)
 echo "Total size of backups: "$(du -sh $1)
