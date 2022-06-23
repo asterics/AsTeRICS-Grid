28e73af46aa27b103a8441fad15391f1589c7d7c
Benjamin Klaus <klaus@technikum-wien.at>
2022-06-23 13:07:23 +0200

    made caching of urls in service worker fail-safe, added possibility to add files to cache from client

--- serviceWorker.js
+++ serviceWorker.js
@@ -10,15 +10,23 @@ self.__WB_DISABLE_DEV_LOGS = true;
 
 self.addEventListener('install', (event) => {
     console.log('installing service worker ...');
-    let urls = ["/","/latest/","index.html","app/build/5.bundle.js","app/build/6.bundle.js","app/build/7.bundle.js","app/build/asterics-grid.bundle.js","app/build/JSZip.bundle.js","app/build/vendors~hls.js.bundle.js","app/build/vendors~html2canvas.bundle.js","app/build/vendors~jspdf.bundle.js","app/build/vendors~JSZip.bundle.js","app/css/custom.css","app/css/fontawesome/css/all.css","app/css/fontawesome/webfonts/fa-brands-400.eot","app/css/fontawesome/webfonts/fa-brands-400.svg","app/css/fontawesome/webfonts/fa-brands-400.ttf","app/css/fontawesome/webfonts/fa-brands-400.woff","app/css/fontawesome/webfonts/fa-brands-400.woff2","app/css/fontawesome/webfonts/fa-regular-400.eot","app/css/fontawesome/webfonts/fa-regular-400.svg","app/css/fontawesome/webfonts/fa-regular-400.ttf","app/css/fontawesome/webfonts/fa-regular-400.woff","app/css/fontawesome/webfonts/fa-regular-400.woff2","app/css/fontawesome/webfonts/fa-solid-900.eot","app/css/fontawesome/webfonts/fa-solid-900.svg","app/css/fontawesome/webfonts/fa-solid-900.ttf","app/css/fontawesome/webfonts/fa-solid-900.woff","app/css/fontawesome/webfonts/fa-solid-900.woff2","app/css/images/ui-icons_444444_256x240.png","app/css/images/ui-icons_555555_256x240.png","app/css/images/ui-icons_777620_256x240.png","app/css/images/ui-icons_777777_256x240.png","app/css/images/ui-icons_cc0000_256x240.png","app/css/images/ui-icons_ffffff_256x240.png","app/css/jquery-ui.css","app/css/skeleton.css","app/examples/default.grd.json","app/img/app-icons/Icon-144.png","app/img/app-icons/Icon-192.png","app/img/app-icons/Icon-36.png","app/img/app-icons/Icon-48.png","app/img/app-icons/Icon-512.png","app/img/app-icons/Icon-72.png","app/img/app-icons/Icon-96.png","app/img/asterics-grid-icon-inkscape.svg","app/img/asterics-grid-icon-raw.svg","app/img/asterics-grid-icon.png","app/img/asterics-grid-iconHQ.png","app/img/asterics_icon.png","app/img/CBT_OS-logo_2Color-H.png","app/img/favicon.ico","app/img/logo_with_border.png","app/img/ma23-gefoerdertvon.jpg","app/index.html","app/lib/gridList.js","app/lib/jquery-ui.min.js","app/lib/jquery.contextMenu.min.js","app/lib/jquery.gridList.js","app/lib/jquery.min.js","app/lib/jquery.ui.position.min.js","app/lib/jquery.ui.touchpunch.min.js","app/lib/loglevel.min.js","app/lib/object-model.min.js","app/lib/pouchdb-7.0.0.min.js","app/lib/responsive-voice.js","app/lib/sjcl.min.js","app/lib/workbox-sw.js","app/manifest.appcache","app/manifest.webmanifest","app/polyfill/core.min.js","app/polyfill/core.min.js.map","app/polyfill/fetch.js","app/polyfill/url-search-params-polyfill.min.js","app/privacy_de.html","app/privacy_en.html"];
+    let urls = ["/","/latest/","index.html","app/build/5.bundle.js","app/build/6.bundle.js","app/build/7.bundle.js","app/build/asterics-grid.bundle.js","app/build/JSZip.bundle.js","app/build/vendors~hls.js.bundle.js","app/build/vendors~html2canvas.bundle.js","app/build/vendors~jspdf.bundle.js","app/build/vendors~JSZip.bundle.js","app/css/bootstrap-grid.css","app/css/custom.css","app/css/fontawesome/css/all.css","app/css/fontawesome/webfonts/fa-brands-400.eot","app/css/fontawesome/webfonts/fa-brands-400.svg","app/css/fontawesome/webfonts/fa-brands-400.ttf","app/css/fontawesome/webfonts/fa-brands-400.woff","app/css/fontawesome/webfonts/fa-brands-400.woff2","app/css/fontawesome/webfonts/fa-regular-400.eot","app/css/fontawesome/webfonts/fa-regular-400.svg","app/css/fontawesome/webfonts/fa-regular-400.ttf","app/css/fontawesome/webfonts/fa-regular-400.woff","app/css/fontawesome/webfonts/fa-regular-400.woff2","app/css/fontawesome/webfonts/fa-solid-900.eot","app/css/fontawesome/webfonts/fa-solid-900.svg","app/css/fontawesome/webfonts/fa-solid-900.ttf","app/css/fontawesome/webfonts/fa-solid-900.woff","app/css/fontawesome/webfonts/fa-solid-900.woff2","app/css/images/ui-icons_444444_256x240.png","app/css/images/ui-icons_555555_256x240.png","app/css/images/ui-icons_777620_256x240.png","app/css/images/ui-icons_777777_256x240.png","app/css/images/ui-icons_cc0000_256x240.png","app/css/images/ui-icons_ffffff_256x240.png","app/css/jquery-ui.css","app/css/skeleton.css","app/examples/default.grd.json","app/img/app-icons/Icon-144.png","app/img/app-icons/Icon-192.png","app/img/app-icons/Icon-36.png","app/img/app-icons/Icon-48.png","app/img/app-icons/Icon-512.png","app/img/app-icons/Icon-72.png","app/img/app-icons/Icon-96.png","app/img/asterics-grid-icon-inkscape.svg","app/img/asterics-grid-icon-raw.svg","app/img/asterics-grid-icon.png","app/img/asterics-grid-iconHQ.png","app/img/asterics_icon.png","app/img/CBT_OS-logo_2Color-H.png","app/img/favicon.ico","app/img/logo_with_border.png","app/img/ma23-gefoerdertvon.jpg","app/index.html","app/lib/gridList.js","app/lib/jquery-ui.min.js","app/lib/jquery.contextMenu.min.js","app/lib/jquery.gridList.js","app/lib/jquery.min.js","app/lib/jquery.ui.position.min.js","app/lib/jquery.ui.touchpunch.min.js","app/lib/loglevel.min.js","app/lib/object-model.min.js","app/lib/pouchdb-7.0.0.min.js","app/lib/responsive-voice.js","app/lib/sjcl.min.js","app/lib/workbox-sw.js","app/manifest.appcache","app/manifest.webmanifest","app/polyfill/core.min.js","app/polyfill/core.min.js.map","app/polyfill/fetch.js","app/polyfill/url-search-params-polyfill.min.js","app/privacy_de.html","app/privacy_en.html"];
     const cacheName = workbox.core.cacheNames.runtime;
     let promise = caches.delete(cacheName).then(() => {
         return caches.open(cacheName);
     }).then((cache) => {
-        return cache.addAll(urls);
+        return Promise.all(
+            urls.map(function (url) {
+                return cache.add(url).catch(function (reason) {
+                    console.warn(`failed to fetch "${url}" in Service Worker.`);
+                    console.warn(reason);
+                    return Promise.resolve();
+                });
+            })
+        );
     });
     event.waitUntil(promise);
-    self.skipWaiting()
+    self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
@@ -27,6 +35,16 @@ self.addEventListener('activate', event => {
         clients.forEach(client => client.postMessage({activated: true}));
     });
     console.log('Service Worker active! Version: https://github.com/asterics/AsTeRICS-Grid/releases/tag/#ASTERICS_GRID_VERSION#');
+});
+
+self.addEventListener('message', (event) => {
+    if (event.data && event.data.urlToAdd) {
+        console.log(`adding ${event.data.urlToAdd} to cache...`);
+        const cacheName = workbox.core.cacheNames.runtime;
+        caches.open(cacheName).then((cache) => {
+            cache.add(event.data.urlToAdd);
+        });
+    }
 });
 
 workbox.routing.registerRoute(({url, request, event}) => {
