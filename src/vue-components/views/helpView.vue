0a6183173ce5a2b30cb8dab8abab7b538d83176d
Benjamin Klaus <klaus@technikum-wien.at>
2022-09-06 14:34:59 +0200

    added translations for help view

--- src/vue-components/views/helpView.vue
+++ src/vue-components/views/helpView.vue
@@ -3,16 +3,16 @@
         <header-icon full-header="true"></header-icon>
         <div class="container ms-0">
             <div class="row ps-2 ps-sm-3 ps-md-4 col-12 col-md-10 col-xl-8 mt-0">
-                <h1>Help</h1>
+                <h1>{{ $t('help') }}</h1>
                 <div>
-                    <span>The following ressources for help on AsTeRICS Grid are available</span>:
+                    <span>{{ $t('theFollowingRessourcesForHelpOnAstericsGrid') }}</span>:
                 </div>
                 <ul>
-                    <li><a href="https://www.asterics.eu/manuals/asterics-grid/" target="_blank">User manual <span>({{ 'lang.en' | translate }})</span></a></li>
-                    <li v-if="currentLang !== 'es'"><a href="https://aulaabierta.arasaac.org/en/asterics-grid_home" target="_blank">ARASAAC tutorial <span>({{ 'lang.en' | translate }})</span></a></li>
-                    <li v-if="currentLang === 'es'"><a href="https://aulaabierta.arasaac.org/asterics-grid_inicio" target="_blank">ARASAAC tutorial <span>({{ 'lang.es' | translate }})</span></a></li>
-                    <li><a href="https://www.youtube.com/playlist?list=PL0UXHkT03dGrIHldlEKR0ZWfNMkShuTNz" target="_blank">Tutorial videos on YouTube <span>({{ 'lang.de' | translate }})</span></a></li>
-                    <li><a href="https://www.youtube.com/channel/UCVIQETg6tIMu3SdEHaIftIw/videos" target="_blank">Tutorial videos on YouTube by Jim Luther <span>({{ 'lang.en' | translate }})</span></a></li>
+                    <li><a href="https://www.asterics.eu/manuals/asterics-grid/" target="_blank">{{ $t('userManual') }} <span>({{ 'lang.en' | translate }})</span></a></li>
+                    <li v-if="currentLang !== 'es'"><a href="https://aulaabierta.arasaac.org/en/asterics-grid_home" target="_blank">{{ $t('arasaacTutorial') }} <span>({{ 'lang.en' | translate }})</span></a></li>
+                    <li v-if="currentLang === 'es'"><a href="https://aulaabierta.arasaac.org/asterics-grid_inicio" target="_blank">{{ $t('arasaacTutorial') }} <span>({{ 'lang.es' | translate }})</span></a></li>
+                    <li><a href="https://www.youtube.com/playlist?list=PL0UXHkT03dGrIHldlEKR0ZWfNMkShuTNz" target="_blank"><span>{{ $t('tutorialVideosOnYoutube') }}</span> <span>({{ 'lang.de' | translate }})</span></a></li>
+                    <li><a href="https://www.youtube.com/channel/UCVIQETg6tIMu3SdEHaIftIw/videos" target="_blank"><span>{{ $t('tutorialVideosOnYoutube') }}</span> <span>(Jim Luther, {{ 'lang.en' | translate }})</span></a></li>
                 </ul>
             </div>
         </div>
