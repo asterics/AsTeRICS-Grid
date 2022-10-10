23233038bc5faccef58467c5cbbd47490a9c8df7
Benjamin Klaus <klaus@technikum-wien.at>
2022-10-10 10:57:14 +0200

    changed validation of username, maxlength is 16 according to this regex: https://github.com/colinskow/superlogin/blob/master/lib/user.js#L14 (not needed by couchdb)

--- src/vue-components/views/registerView.vue
+++ src/vue-components/views/registerView.vue
@@ -6,7 +6,7 @@
             <form autocomplete="off" onsubmit="event.preventDefault()" style="margin-bottom: 0.5em">
                 <div class="srow">
                     <label for="inputUser" class="two columns"><span class="desktop-right inputlabel">{{ $t('username') }}</span></label>
-                    <input type="text" name="username" v-model="user" id="inputUser" class="six columns" @change="validateUsername" v-debounce="300" v-focus=""/>
+                    <input type="text" name="username" v-model="user" id="inputUser" class="six columns" @change="validateUsername" v-debounce="300" v-focus="" maxlength="16"/>
                     <div class="three columns" v-show="user != null && usernameValid == false">
                         <i style="color: red;" class="fas fa-times"/> <span>{{usernameValidationCode | translate}}</span>
                     </div>
