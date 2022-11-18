17995623d7df5259eea25f2a36c2b7236814b25b
Benjamin Klaus <klaus@technikum-wien.at>
2022-11-18 14:21:42 +0100

    make "speak continuous" default for global grid, see https://github.com/asterics/AsTeRICS-Grid/issues/22#issuecomment-1319809308

--- src/js/model/GridElementCollect.js
+++ src/js/model/GridElementCollect.js
@@ -19,7 +19,7 @@ class GridElementCollect extends GridElement.extend({
         props.mode = GridElementCollect.MODE_AUTO;
         props.type = GridElement.ELEMENT_TYPE_COLLECT;
         props.textElemSizeFactor = 1.5;
-        props.actions = props.actions || [new GridActionCollectElement({action: GridActionCollectElement.COLLECT_ACTION_SPEAK}), new GridActionPredict({suggestOnChange: true})];
+        props.actions = props.actions || [new GridActionCollectElement({action: GridActionCollectElement.COLLECT_ACTION_SPEAK_CONTINUOUS}), new GridActionPredict({suggestOnChange: true})];
         super(props);
     }
 }
