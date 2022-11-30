04ce53422dc7248b3797dd3ccc096ab19c5ef742
Benjamin Klaus <klaus@technikum-wien.at>
2022-11-30 10:25:17 +0100

    updated script for testing for duplicated metadata

--- scripts/testDuplicatedMetadata.js
+++ scripts/testDuplicatedMetadata.js
@@ -1,6 +1,6 @@
 if (!process.argv[2] || !process.argv[2].trim()) {
     console.log('USAGE:');
-    console.log("node testDuplicatedMetadata.js <COUCHDB_URL> ['compact']");
+    console.log("node testDuplicatedMetadata.js <COUCHDB_URL>");
     console.log("----");
     console.log("Examples:");
     console.log("node testDuplicatedMetadata.js http://admin:admin@localhost:5984 ... for analyzing duplicated metadata");
@@ -16,17 +16,32 @@ const nano = require('nano')({
 
 async function main() {
     const dblist = await nano.db.list().catch(err => console.log(err));
+    let countNone = 0;
+    let countSingle = 0;
+    let countDuplicated = 0;
+    let duplicatedNames = [];
     for (const dbName of dblist) {
-        if (dbName === "sl-users") {
-            continue;
-        }
-        if (dbName === "asterics-grid-data$klues") {
+        if (dbName !== "sl-users") {
             let db = nano.db.use(dbName);
             let doclist = await db.list();
-            let metadataDocs = doclist.rows.filter(doc => doc.modelName === 'MetaData');
-            console.warn(`${dbName} has ${metadataDocs.length} metadata docs!`);
+            let metadataDocs = doclist.rows.filter(doc => doc.id.startsWith('meta-data'));
+            if (metadataDocs.length === 0) {
+                console.warn(`${dbName} has NO metadata docs!!!!!!!!`);
+                countNone++;
+            } else if (metadataDocs.length === 1) {
+                countSingle++;
+            } else if (metadataDocs.length > 1) {
+                console.warn(`${dbName} has ${metadataDocs.length} metadata docs!`);
+                countDuplicated++;
+                duplicatedNames.push(dbName);
+            }
         }
     }
+    console.log("---------- RESULT ---------");
+    console.log(`No metadata: ${countNone}`);
+    console.log(`Single metadata: ${countSingle}`);
+    console.log(`Duplicated metadata: ${countDuplicated}`);
+    console.log(`Duplicated db names: ${JSON.stringify(duplicatedNames)}`);
 }
 
 main();
