0a2feeefefc3a4795c333d870185218301a554ea
Benjamin Klaus <klaus@technikum-wien.at>
2023-05-04 16:18:35 +0200

    added bundles and appcache for release release-2023-05-04-16.18/+0200

--- serviceWorker.js
+++ serviceWorker.js
@@ -34,7 +34,7 @@ self.addEventListener('activate', event => {
     self.clients.matchAll().then(clients => {
         clients.forEach(client => client.postMessage({activated: true}));
     });
-    console.log('Service Worker active! Version: https://github.com/asterics/AsTeRICS-Grid/releases/tag/#ASTERICS_GRID_VERSION#');
+    console.log('Service Worker active! Version: https://github.com/asterics/AsTeRICS-Grid/releases/tag/release-2023-05-04-16.18/+0200');
 });
 
 self.addEventListener('message', (event) => {
