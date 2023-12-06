5b92d9f9612a1e2c962e52afbdb607b6226fa7f1
Alija Sabic <sabic.alija@gmail.com>
2023-12-06 14:25:54 +0100

    Create modal wrapper component

---
+++ src/vue-components/modals/modal.vue
@@ -0,0 +1,22 @@
+<template>
+    <dialog ref="modal">
+        <div class="modal-header"><slot name="header"></slot></div>
+        <div class="modal-body"><slot></slot></div>
+        <div class="modal-footer"><slot name="footer"></slot></div>
+    </dialog>
+</template>
+
+<script>
+export default {
+    methods: {
+        open() {
+            this.$refs.modal.showModal();
+        },
+        close() {
+            this.$refs.modal.close();
+        }
+    }
+};
+</script>
+
+<style scoped></style>
