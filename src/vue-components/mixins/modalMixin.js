2f964ae828609b792275a4622f7ab8635399bc9d
Alija Sabic <sabic.alija@gmail.com>
2023-12-06 14:26:05 +0100

    Create modal mixin

---
+++ src/vue-components/mixins/modalMixin.js
@@ -0,0 +1,18 @@
+export const modalMixin = {
+    methods: {
+        openModal() {
+            // Test if component using the mixin has the vue component Modal.vue as root element.
+            if (this.$children.length > 0 && this.$children[0].$el.tagName === 'DIALOG') {
+                const modal = this.$children[0];
+                modal.open();
+            }
+        },
+        closeModal() {
+            // Test if component using the mixin has the vue component Modal.vue as root element.
+            if (this.$children.length > 0 && this.$children[0].$el.tagName === 'DIALOG') {
+                const modal = this.$children[0];
+                modal.close();
+            }
+        }
+    }
+};
