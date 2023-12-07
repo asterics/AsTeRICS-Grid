94e073612a183e32c66635632b3e9c4db1939341
Benjamin Klaus <klaus@technikum-wien.at>
2023-12-07 11:13:54 +0100

    reverted release version to placeholder

--- serviceWorker.js
+++ serviceWorker.js
@@ -39,7 +39,7 @@ self.addEventListener('install', (event) => {
 self.addEventListener('activate', event => {
     clients.claim();
     sendToClients({type: constants.SW_EVENT_ACTIVATED, activated: true});
-    console.log('Service Worker active! Version: https://github.com/asterics/AsTeRICS-Grid/releases/tag/release-beta-2023-12-07-11.12/+0100');
+    console.log('Service Worker active! Version: https://github.com/asterics/AsTeRICS-Grid/releases/tag/#ASTERICS_GRID_VERSION#');
 });
 
 self.addEventListener('message', (event) => {
