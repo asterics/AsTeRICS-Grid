7db537f3cfdfc4d95bae7f10a9525531c7b84ac1
Martin Deinhofer <deinhofer.m@gmail.com>
2023-12-15 14:06:42 +0100

    removed mehtod getRestUrl as it's not needed

--- src/js/service/restService.js
+++ src/js/service/restService.js
@@ -39,26 +39,4 @@ restService.doAction = async function (action) {
     }
 };
 
-restService.getRestURL = function (userUri) {
-    if (!userUri) {
-        userUri =
-            window.location.hostname.indexOf('grid.asterics.eu') > -1
-                ? 'http://127.0.0.1:8080'
-                : 'http://' + window.location.hostname + ':8080';
-    }
-
-    if (userUri.indexOf('http') === -1) {
-        userUri = 'http://' + userUri;
-    }
-
-    let parser = document.createElement('a');
-    parser.href = userUri;
-    parser.pathname = '/rest/items/';
-    if (!parser.port) {
-        parser.port = 8080;
-    }
-
-    return parser.href;
-};
-
 export {restService};
