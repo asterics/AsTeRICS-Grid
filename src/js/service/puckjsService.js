fa0611faa96097ec13a5b1d83519904bb2499b35
Martin Deinhofer <deinhofer.m@gmail.com>
2024-04-11 17:26:11 +0200

    changed action to use uart.js and connect function

--- src/js/service/puckjsService.js
+++ src/js/service/puckjsService.js
@@ -1,13 +1,36 @@
+import {MainVue} from "../vue/mainVue";
+
 let puckjsService = {};
 
 puckjsService.doAction = function (action) {
 
-    import(/* webpackPrefetch: 0 */ '/app/lib/puck.js').then((
+    import(/* webpackPrefetch: 0 */ '/app/lib/uart.js').then((
     ) => {
         let cmdString = `${action.puckjsCmd}\n`;
         log.debug(cmdString);
 
-        Puck.write(cmdString);
+        UART.connect(function(connection) {
+            if (!connection) {
+                MainVue.setTooltip(i18nService.t("UART action failed", error), {
+                    revertOnClose: true,
+                    timeout: 5000
+                });
+                return;
+            }
+            connection.on('data', function(d) {
+                console.log('UART got data: '+d);
+            });
+            connection.on('close', function() {
+                console.log('UART closed: ');
+            });
+            // actually write the grid action command string to the device
+            connection.write(cmdString, function() {
+            });
+        });
+        /*
+        //Using UART.write remembers the connection variable but does not provide a callback for error handling
+        UART.write(cmdString);
+         */
     });
 };
 
