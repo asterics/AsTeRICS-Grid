b0e9bd3732a8967d42773d55b283cefc6a34cc3c
Votre Nom <tchjohanna@icloud.com>
2024-05-21 20:47:13 +0200

    Improve editing mode button and visibility of current mode status #385

--- src/vue-components/components/editElementHeader.vue
+++ src/vue-components/components/editElementHeader.vue
@@ -6,10 +6,10 @@
                     {{ header }}
                 </h1>
             </div>
-            <a v-if="openHelpFn" class="col-2 col-sm-1 col-md black order-md-3" href="javascript:;" @click="openHelpFn"><i class="fas fa-question-circle"></i></a>
-            <a v-if="closeFn" id="closeLink" :title="$t('close')" class="col-2 col-sm-1 col-md black order-md-4" href="javascript:;" @click="closeFn"><i class="fas fa-times"/></a>
+            <a v-if="openHelpFn" class="col-2 col-sm-1 col-md black order-md-3" href="javascript:;" @click="openHelpFn" :title="helpTooltip"><i class="fas fa-question-circle"></i></a>
+            <a v-if="closeFn" id="closeLink" :title="closeTooltip" class="col-2 col-sm-1 col-md black order-md-4" href="javascript:;" @click="closeFn"><i class="fas fa-times"/></a>
             <div class="col-12 col-md-5 d-flex align-items-center order-md-2 mt-2 mt-md-0" v-if="gridElement">
-                <div v-if="gridElement.type === GridElement.ELEMENT_TYPE_NORMAL">
+                <div :class="editingClass">
                     <img class="me-1" v-if="gridElement.image && (gridElement.image.data || gridElement.image.url)" height="30" :src="gridElement.image.data || gridElement.image.url"/>
                     <span>{{ gridElement.label | extractTranslation }}</span>
                 </div>
@@ -22,22 +22,28 @@
 </template>
 
 <script>
-
-    import {GridElement} from "../../js/model/GridElement.js";
+    import { GridElement } from "../../js/model/GridElement.js";
 
     export default {
         props: ["header", "gridElement", "openHelpFn", "closeFn"],
         data() {
             return {
-                GridElement: GridElement
+                GridElement: GridElement,
+                isEditing: false // Assuming there's a way to determine the editing mode
             }
         },
-        methods: {
-        },
-        mounted() {
-        },
+        computed: {
+            editingClass() {
+                return this.isEditing ? 'editing-on editing-on-cursor' : 'editing-off';
+            },
+            helpTooltip() {
+                return this.isEditing ? 'Click to get help in editing mode' : 'Click to get help in view mode';
+            },
+            closeTooltip() {
+                return this.isEditing ? 'Click to exit editing mode' : 'Click to close';
+            }
+        }
     }
 </script>
 
-<style scoped>
-</style>
+
