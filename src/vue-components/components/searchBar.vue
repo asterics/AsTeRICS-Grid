0b92b5071996df4d2dd94c07f116cb6a0cbed0be
Benjamin Klaus <benjamin@asterics-foundation.org>
2024-09-06 05:36:27 +0200

    added clear button to search bar

--- src/vue-components/components/searchBar.vue
+++ src/vue-components/components/searchBar.vue
@@ -2,7 +2,10 @@
     <div class="searchContainer" style="position: relative; width: 100%">
         <label class="sr-only" for="searchBar">{{ $t('search') }}</label>
         <input id="searchBar" type="search" v-model="currentValue" :placeholder="$t('search')" @input="changed" @change="changed" style="width: 100%;">
-        <button :title="$t('search')" style="" @click="changed"><i class="fas fa-search"></i></button>
+        <div class="barButtons">
+            <button :title="$t('clear')" @click="clear" style="background-color: transparent; outline: none;"><i class="fas fa-times"></i></button>
+            <button :title="$t('search')" @click="changed"><i class="fas fa-search"></i></button>
+        </div>
     </div>
 </template>
 
@@ -23,6 +26,10 @@ export default {
         changed() {
             this.$emit('input', this.currentValue);
             this.$emit('change', this.currentValue);
+        },
+        clear() {
+            this.currentValue = '';
+            this.changed();
         }
     },
     mounted() {
@@ -31,9 +38,13 @@ export default {
 </script>
 
 <style scoped>
-.searchContainer button {
+.searchContainer .barButtons {
     position: absolute;
     right: 0;
+    top: 0;
+}
+
+.barButtons button {
     line-height: 1.5;
     margin: 0;
     padding: 0 1em;
