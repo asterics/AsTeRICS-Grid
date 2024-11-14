dcacd79dd68cee931ede431fc9083d9f671da32e
Alija Sabic <sabic.alija@gmail.com>
2024-11-14 16:24:26 +0100

    Update modal wrapper component: incorporate style classes of previous modal architecture as an intermediate step

--- src/vue-components/modals/modal.vue
+++ src/vue-components/modals/modal.vue
@@ -1,32 +1,55 @@
 <template>
     <dialog ref="modal">
-        <div class="modal-header">
-            <slot name="header">
-                <h1 v-if="title">{{ title }}</h1>
-                <button v-if="helpFn" class="remove-btn-skeleton help" @click="helpFn" :aria-label="ariaLabelHelp">
-                    <i class="fas fa-question-circle fa-lg" aria-hidden="true"></i>
-                </button>
-                <button class="remove-btn-skeleton close" @click="close" :aria-label="ariaLabelEsc">
-                    <i class="fas fa-times fa-lg" aria-hidden="true"></i>
-                </button>
-            </slot>
-            <!-- TODO: insert label/esc, aria-hidden (https://getbootstrap.com/docs/4.0/components/modal/#modal-components) -->
-        </div>
-        <div class="modal-body"><slot></slot></div>
-        <div class="modal-footer">
-            <slot name="footer">
-                <button @click="close" @keydown.esc="close" :aria-label="ariaLabelCancel" :title="titleCancel">
-                    <i class="fas fa-times" aria-hidden="true"></i>
-                    {{ $t('cancel') }}
-                </button>
-                <slot name="ok-button">
-                    <button @click="open" @keydown.ctrl.enter="open" :aria-label="ariaLabelOk" :title="titleOk">
-                        <i class="fas fa-check" aria-hidden="true"></i>
-                        {{ $t('ok') }}
-                    </button>
-                </slot>
-                <slot name="footer-appendix"></slot>
-            </slot>
+        <div class="modal">
+            <div class="modal-mask">
+                <div class="modal-wrapper">
+                    <div class="modal-container">
+                        <div class="modal-header">
+                            <slot name="header">
+                                <h1 v-if="title">{{ title }}</h1>
+                                <button
+                                    v-if="helpFn"
+                                    class="remove-btn-skeleton help"
+                                    @click="helpFn"
+                                    :aria-label="ariaLabelHelp"
+                                >
+                                    <i class="fas fa-question-circle fa-lg" aria-hidden="true"></i>
+                                </button>
+                                <button class="remove-btn-skeleton close" @click="close" :aria-label="ariaLabelEsc">
+                                    <i class="fas fa-times fa-lg" aria-hidden="true"></i>
+                                </button>
+                            </slot>
+                            <!-- TODO: insert label/esc, aria-hidden (https://getbootstrap.com/docs/4.0/components/modal/#modal-components) -->
+                        </div>
+                        <div class="modal-body"><slot></slot></div>
+                        <div class="modal-footer">
+                            <slot name="footer">
+                                <button
+                                    @click="close"
+                                    @keydown.esc="close"
+                                    :aria-label="ariaLabelCancel"
+                                    :title="titleCancel"
+                                >
+                                    <i class="fas fa-times" aria-hidden="true"></i>
+                                    {{ $t('cancel') }}
+                                </button>
+                                <slot name="ok-button">
+                                    <button
+                                        @click="open"
+                                        @keydown.ctrl.enter="open"
+                                        :aria-label="ariaLabelOk"
+                                        :title="titleOk"
+                                    >
+                                        <i class="fas fa-check" aria-hidden="true"></i>
+                                        {{ $t('ok') }}
+                                    </button>
+                                </slot>
+                                <slot name="footer-appendix"></slot>
+                            </slot>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </dialog>
 </template>
@@ -88,60 +111,28 @@ button {
     cursor: pointer;
 }
 
-dialog {
-    max-width: 920px;
-    margin: auto;
-    padding: 2rem 4rem;
-
-    .modal-header,
-    .modal-body,
-    .modal-footer {
-        display: flex;
-    }
-
-    .modal-header,
-    .modal-footer {
-        flex-flow: row nowrap;
-        align-items: center;
-    }
-
-    .modal-body {
-        flex-flow: row wrap;
-    }
-
-    .modal-header {
-        h1 {
-            font-weight: 600;
-            font-size: 2.2rem;
-            flex-grow: 6;
-        }
-    }
-
-    .modal-footer {
-        > * {
-            flex-grow: 1;
-            margin: 0.25rem 0.5rem;
-            min-width: 100px;
-        }
-    }
-
-    .modal-header,
-    .modal-footer {
-        justify-content: space-between;
+.modal-header {
+    display: flex;
+    h1 {
+        font-weight: 600;
+        font-size: 2.2rem;
+        flex-grow: 6;
     }
 }
 
-@media screen and (max-width: 768px) {
-    /* Rules for Smartphones */
+.modal-footer {
+    display: flex;
+    > * {
+        flex-grow: 1;
+        margin: 0.25rem 0.5rem;
+        min-width: 100px;
+    }
 }
 
-@media screen and (max-width: 1024px) {
-    /* Rules for Tablets */
-    dialog {
-        .modal-footer {
-            flex-flow: row wrap;
-        }
-    }
+.modal-header,
+.modal-footer {
+    display: flex;
+    justify-content: space-between;
 }
 
 .remove-btn-skeleton {
@@ -161,32 +152,4 @@ dialog {
     text-align: unset;
     transition: unset;
 }
-
-/* .modal-header,
-.modal-body,
-.modal-footer {
-    /* display: flex; */
-/* flex-flow: row nowrap;
-    align-content: space-between;
-} */
-
-/* .modal-header .close {
-    appearance: none;
-}
-
-.modal-header .close {
-    display: unset;
-    padding: unset;
-    line-height: unset;
-    font-size: unset;
-    font-weight: unset;
-    text-align: unset;
-    border: unset;
-    outline: unset;
-    border-radius: unset;
-    box-shadow: unset;
-    background-color: unset;
-    color: unset;
-    transition: unset;
-} */
 </style>
