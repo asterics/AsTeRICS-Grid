0221fdef16f1577dc97f3e8d6b81e2d3c4b1fe9a
Alija Sabic <sabic.alija@gmail.com>
2024-11-17 22:26:32 +0100

    Use new modal (mouseModal)

--- src/vue-components/modals/input/mouseModal.vue
+++ src/vue-components/modals/input/mouseModal.vue
@@ -1,79 +1,56 @@
 <template>
-    <div class="modal">
-        <div class="modal-mask">
-            <div class="modal-wrapper">
-                <div class="modal-container" @keydown.27="cancel()" @keydown.enter="save()">
-                    <a class="inline close-button" href="javascript:void(0);" @click="cancel()"><i class="fas fa-times"/></a>
-                    <a class="close-button" href="javascript:;" @click="openHelp()"><i class="fas fa-question-circle"></i></a>
-                    <div class="modal-header">
-                        <h1 name="header">{{ $t('mousetouchInput') }}</h1>
-                    </div>
-
-                    <div class="modal-body" v-if="inputConfig">
-                        <div class="srow">
-                            <div class="twelve columns">
-                                <input v-focus type="checkbox" id="enableClick" v-model="inputConfig.mouseclickEnabled"/>
-                                <label class="inline" for="enableClick">{{ $t('selectWithMouseClickOrTap') }}</label>
-                            </div>
-                        </div>
-                        <div class="srow">
-                            <div class="twelve columns">
-                                <input v-focus type="checkbox" id="enableDoubleClick" v-model="inputConfig.mouseDoubleClickEnabled"/>
-                                <label class="inline" for="enableDoubleClick">{{ $t('selectWithDoubleClickOrDoubleTap') }}</label>
-                            </div>
-                        </div>
-                        <div class="srow" >
-                            <div class="twelve columns">
-                                <input v-focus type="checkbox" id="enableHover" v-model="inputConfig.hoverEnabled"/>
-                                <label class="inline" for="enableHover">{{ $t('enableHovering') }}</label>
-                            </div>
-                        </div>
-                        <div class="srow" v-show="inputConfig.hoverEnabled">
-                            <label class="four columns" for="inHoverTime">{{ $t('hoverTimeMs') }}</label>
-                            <input type="range" id="inHoverTime" v-model.number="inputConfig.hoverTimeoutMs" min="0" max="5000" step="100"/>
-                            <input type="number" v-model.number="inputConfig.hoverTimeoutMs" min="0" max="5000" step="100"/>
-                        </div>
-                        <div class="srow" v-show="inputConfig.hoverEnabled">
-                            <div class="twelve columns">
-                                <input v-focus type="checkbox" id="hoverHideCursor" v-model="inputConfig.hoverHideCursor"/>
-                                <label class="inline" for="hoverHideCursor">{{ $t('hideCursor') }}</label>
-                            </div>
-                            <div class="twelve columns">
-                                <input type="checkbox" id="chkDisableHoverpane" v-model="inputConfig.hoverDisableHoverpane"/>
-                                <label for="chkDisableHoverpane">{{ $t('disableHoverPane') }}</label>
-                            </div>
-                        </div>
-                        <div class="mb-4"></div>
-                        <accordion :acc-label="$t('ADVANCED_SETTINGS')" acc-label-type="h2" acc-background-color="white">
-                            <div class="srow">
-                                <div class="twelve columns">
-                                    <input v-focus type="checkbox" id="mousedown" v-model="inputConfig.mouseDownInsteadClick"/>
-                                    <label class="inline" for="mousedown">{{ $t('directlySelectElementOnPressingMouseButton') }}</label>
-                                </div>
-                            </div>
-                        </accordion>
-                        <accordion :acc-label="$t('generalInputSettings')" acc-label-type="h2" acc-background-color="white">
-                            <global-input-options :input-config="inputConfig"/>
-                        </accordion>
-                        <accordion :acc-label="$t('TEST_CONFIGURATION')" acc-label-type="h2" acc-background-color="white" @open="testOpen = true; initTest()" @close="testOpen = false; stopTest()">
-                            <test-area :selected-element="selectedTestElement"></test-area>
-                        </accordion>
-                    </div>
-
-                    <div class="modal-footer">
-                        <div class="button-container srow">
-                            <button @click="cancel()" class="four columns offset-by-four">
-                                <i class="fas fa-times"/> <span>{{ $t('cancel') }}</span>
-                            </button>
-                            <button @click="save()" class="four columns">
-                                 <i class="fas fa-check"/> <span>{{ $t('ok') }}</span>
-                            </button>
-                        </div>
-                    </div>
+    <modal :title="$t('mousetouchInput')" @ok="save" @close="cancel" :help-fn="openHelp">
+        <template #default v-if="inputConfig">
+            <div class="srow">
+                <div class="twelve columns">
+                    <input v-focus type="checkbox" id="enableClick" v-model="inputConfig.mouseclickEnabled"/>
+                    <label class="inline" for="enableClick">{{ $t('selectWithMouseClickOrTap') }}</label>
                 </div>
             </div>
-        </div>
-    </div>
+            <div class="srow">
+                <div class="twelve columns">
+                    <input v-focus type="checkbox" id="enableDoubleClick" v-model="inputConfig.mouseDoubleClickEnabled"/>
+                    <label class="inline" for="enableDoubleClick">{{ $t('selectWithDoubleClickOrDoubleTap') }}</label>
+                </div>
+            </div>
+            <div class="srow" >
+                <div class="twelve columns">
+                    <input v-focus type="checkbox" id="enableHover" v-model="inputConfig.hoverEnabled"/>
+                    <label class="inline" for="enableHover">{{ $t('enableHovering') }}</label>
+                </div>
+            </div>
+            <div class="srow" v-show="inputConfig.hoverEnabled">
+                <label class="four columns" for="inHoverTime">{{ $t('hoverTimeMs') }}</label>
+                <input type="range" id="inHoverTime" v-model.number="inputConfig.hoverTimeoutMs" min="0" max="5000" step="100"/>
+                <input type="number" v-model.number="inputConfig.hoverTimeoutMs" min="0" max="5000" step="100"/>
+            </div>
+            <div class="srow" v-show="inputConfig.hoverEnabled">
+                <div class="twelve columns">
+                    <input v-focus type="checkbox" id="hoverHideCursor" v-model="inputConfig.hoverHideCursor"/>
+                    <label class="inline" for="hoverHideCursor">{{ $t('hideCursor') }}</label>
+                </div>
+                <div class="twelve columns">
+                    <input type="checkbox" id="chkDisableHoverpane" v-model="inputConfig.hoverDisableHoverpane"/>
+                    <label for="chkDisableHoverpane">{{ $t('disableHoverPane') }}</label>
+                </div>
+            </div>
+            <div class="mb-4"></div>
+            <accordion :acc-label="$t('ADVANCED_SETTINGS')" acc-label-type="h2" acc-background-color="white">
+                <div class="srow">
+                    <div class="twelve columns">
+                        <input v-focus type="checkbox" id="mousedown" v-model="inputConfig.mouseDownInsteadClick"/>
+                        <label class="inline" for="mousedown">{{ $t('directlySelectElementOnPressingMouseButton') }}</label>
+                    </div>
+                </div>
+            </accordion>
+            <accordion :acc-label="$t('generalInputSettings')" acc-label-type="h2" acc-background-color="white">
+                <global-input-options :input-config="inputConfig"/>
+            </accordion>
+            <accordion :acc-label="$t('TEST_CONFIGURATION')" acc-label-type="h2" acc-background-color="white" @open="testOpen = true; initTest()" @close="testOpen = false; stopTest()">
+                <test-area :selected-element="selectedTestElement"></test-area>
+            </accordion>
+        </template>
+    </modal>
 </template>
 
 <script>
@@ -82,7 +59,8 @@
     import Accordion from "../../components/accordion.vue"
     import InputEventList from "../../components/inputEventList.vue"
     import TestArea from "./testArea.vue"
-    import './../../../css/modal.css';
+    import Modal from '../modal.vue';
+    import { modalMixin } from '../../mixins/modalMixin.js';
     import {InputConfig} from "../../../js/model/InputConfig";
     import {Hover} from "../../../js/input/hovering";
     import {Clicker} from "../../../js/input/clicking";
@@ -91,7 +69,8 @@
 
     export default {
         props: [],
-        components: {GlobalInputOptions, Accordion, InputEventList, TestArea},
+        components: { GlobalInputOptions, Accordion, InputEventList, TestArea, Modal },
+        mixins: [modalMixin],
         data: function () {
             return {
                 inputConfig: null,
