d1dcf5f48a086db57b5df3ef783f6e3fee845fb1
Alija Sabic <sabic.alija@gmail.com>
2024-11-17 19:30:07 +0100

    Update store: add metadata

--- src/js/vue/plugins/store.js
+++ src/js/vue/plugins/store.js
@@ -5,7 +5,10 @@ Vue.use(Vuex);
 
 const store = new Vuex.Store({
     state: {
+        // NOTE: gridData and metadata are objects, thus properties itself are not reactive.
+        //       Always replace the object with a new one to trigger reactivity.
         gridData: null,
+        metadata: null,
         editElementId: null
     },
     mutations: {
@@ -14,6 +17,9 @@ const store = new Vuex.Store({
         },
         setEditElementId(state, payload) {
             state.editElementId = payload;
+        },
+        setMetadata(state, payload) {
+            state.metadata = payload;
         }
     }
 });
