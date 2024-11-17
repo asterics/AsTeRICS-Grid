e2560e61ab34451ad2e0c21682d2bdad4f0583d1
Alija Sabic <sabic.alija@gmail.com>
2024-11-17 19:52:51 +0100

    Update store: add gridInstance

--- src/js/vue/plugins/store.js
+++ src/js/vue/plugins/store.js
@@ -5,21 +5,25 @@ Vue.use(Vuex);
 
 const store = new Vuex.Store({
     state: {
-        // NOTE: gridData and metadata are objects, thus properties itself are not reactive.
+        // NOTE: gridData, metadata, gridInstance are objects, thus properties itself are not reactive.
         //       Always replace the object with a new one to trigger reactivity.
         gridData: null,
         metadata: null,
+        gridInstance: null,
         editElementId: null
     },
     mutations: {
         setGridData(state, payload) {
             state.gridData = payload;
         },
-        setEditElementId(state, payload) {
-            state.editElementId = payload;
-        },
         setMetadata(state, payload) {
             state.metadata = payload;
+        },
+        setGridInstance(state, payload) {
+            state.gridInstance = payload;
+        },
+        setEditElementId(state, payload) {
+            state.editElementId = payload;
         }
     }
 });
