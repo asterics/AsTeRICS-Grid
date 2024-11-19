bf8b0a5a4210c7f26c6a7fe73f7137d27283bf81
Alija Sabic <sabic.alija@gmail.com>
2024-11-20 00:12:52 +0100

    Use new modal (gridLinkModal)

--- src/vue-components/modals/gridLinkModal.vue
+++ src/vue-components/modals/gridLinkModal.vue
@@ -1,68 +1,44 @@
 <template>
-    <div class="modal">
-        <div class="modal-mask">
-            <div class="modal-wrapper">
-                <div class="modal-container" @keyup.27="$emit('close')" @keyup.ctrl.enter="save()">
-                    <a class="inline close-button" href="javascript:void(0);" @click="$emit('close')"><i class="fas fa-times"/></a>
-                    <div class="modal-header">
-                        <h1 name="header">
-                            {{ $t('linkGrids') }}
-                        </h1>
-                    </div>
-
-                    <div class="modal-body" v-if="gridFrom">
-                        <div class="srow">
-                            <div class="four columns">
-                                <div>
-                                    <div>{{gridFrom.label | extractTranslation}}</div>
-                                    <img :src="gridFrom.thumbnail ? gridFrom.thumbnail.data : imageUtil.getEmptyImage()" style="height: 150px; border: 1px solid lightgray"/>
-                                </div>
-                            </div>
-                            <i class="fas fa-2x fa-arrow-right hide-mobile two columns" style="margin-top: 2em"/>
-                            <i class="fas fa-2x fa-arrow-down show-mobile two columns" style="margin: 0.5em 20%"/>
-                            <div class="four columns">
-                                <div>{{gridTo.label | extractTranslation}}</div>
-                                <img :src="gridTo.thumbnail ? gridTo.thumbnail.data : imageUtil.getEmptyImage()" style="height: 150px; border: 1px solid lightgray"/>
-                            </div>
-                        </div>
-                        <div class="srow">
-                            <label class="three columns" for="elementName">{{ $t('connectWithElement') }}</label>
-                            <select id="elementName" class="four columns" v-model="selectedElement">
-                                <option :value="null">{{ $t('createNewElement') }}</option>
-                                <option v-for="elem in gridFrom.gridElements" :value="elem">{{i18nService.getTranslation(elem.label) || i18nService.t('emptyElement')}}</option>
-                            </select>
-                        </div>
-                        <div class="srow" v-show="!selectedElement || !i18nService.getTranslation(selectedElement.label)">
-                            <label class="three columns" for="elementLabel">{{ $t('labelOfNewElement') }}</label>
-                            <input type="text" id="elementLabel" class="four columns" v-model="newElementLabel[i18nService.getContentLang()]" maxlength="35"/>
-                        </div>
-                        <div class="srow" v-show="selectedElement && selectedElement.actions.filter(e => e.modelName === GridActionNavigate.getModelName()).length > 0">
-                            <i class="fas fa-exclamation-triangle"></i>
-                            <span>
-                                <b>{{ $t('hint') }} </b> {{ $t('thisElementAlreadyNavigatesToAnotherGrid') }}
-                            </span>
-                        </div>
-                    </div>
-
-                    <div class="modal-footer">
-                        <div class="button-container srow">
-                            <button class="six columns" @click="$emit('close')" :title="$t('keyboardEsc')">
-                                <i class="fas fa-times"/> <span>{{ $t('cancel') }}</span>
-                            </button>
-                            <button class="six columns" @click="save()" :title="$t('keyboardCtrlEnter')">
-                                <i class="fas fa-check"/> <span>{{ $t('ok') }}</span>
-                            </button>
-                        </div>
+    <modal :title="$t('linkGrids')" @ok="save">
+        <template #default v-if="gridFrom">
+            <div class="srow">
+                <div class="four columns">
+                    <div>
+                        <div>{{gridFrom.label | extractTranslation}}</div>
+                        <img :src="gridFrom.thumbnail ? gridFrom.thumbnail.data : imageUtil.getEmptyImage()" style="height: 150px; border: 1px solid lightgray"/>
                     </div>
                 </div>
+                <i class="fas fa-2x fa-arrow-right hide-mobile two columns" style="margin-top: 2em"/>
+                <i class="fas fa-2x fa-arrow-down show-mobile two columns" style="margin: 0.5em 20%"/>
+                <div class="four columns">
+                    <div>{{gridTo.label | extractTranslation}}</div>
+                    <img :src="gridTo.thumbnail ? gridTo.thumbnail.data : imageUtil.getEmptyImage()" style="height: 150px; border: 1px solid lightgray"/>
+                </div>
             </div>
-        </div>
-    </div>
+            <div class="srow">
+                <label class="three columns" for="elementName">{{ $t('connectWithElement') }}</label>
+                <select id="elementName" class="four columns" v-model="selectedElement">
+                    <option :value="null">{{ $t('createNewElement') }}</option>
+                    <option v-for="elem in gridFrom.gridElements" :value="elem">{{i18nService.getTranslation(elem.label) || i18nService.t('emptyElement')}}</option>
+                </select>
+            </div>
+            <div class="srow" v-show="!selectedElement || !i18nService.getTranslation(selectedElement.label)">
+                <label class="three columns" for="elementLabel">{{ $t('labelOfNewElement') }}</label>
+                <input type="text" id="elementLabel" class="four columns" v-if="newElementLabel" v-model="newElementLabel[i18nService.getContentLang()]" maxlength="35"/>
+            </div>
+            <div class="srow" v-show="selectedElement && selectedElement.actions.filter(e => e.modelName === GridActionNavigate.getModelName()).length > 0">
+                <i class="fas fa-exclamation-triangle"></i>
+                <span>
+                    <b>{{ $t('hint') }} </b> {{ $t('thisElementAlreadyNavigatesToAnotherGrid') }}
+                </span>
+            </div>
+        </template>
+    </modal>
 </template>
 
 <script>
     import {i18nService} from "../../js/service/i18nService";
-    import './../../css/modal.css';
+    import { modalMixin } from '../mixins/modalMixin.js';
     import {imageUtil} from "../../js/util/imageUtil";
     import {GridActionNavigate} from "../../js/model/GridActionNavigate";
     import {GridElement} from "../../js/model/GridElement";
@@ -70,17 +46,23 @@
     import {dataService} from "../../js/service/data/dataService";
 
     export default {
-        props: ['gridFromProp', 'gridToProp'],
+        mixins: [modalMixin],
         data: function () {
             return {
-                gridFrom: null,
-                gridTo: JSON.parse(JSON.stringify(this.gridToProp)),
                 inputText: "",
                 selectedElement: null,
                 newElementLabel: null,
                 GridActionNavigate: GridActionNavigate,
                 imageUtil: imageUtil,
                 i18nService: i18nService
+            }
+        },
+        computed: {
+            gridFrom() {
+                return this.$store.state.gridFrom;
+            },
+            gridTo() {
+                return this.$store.state.gridTo;
             }
         },
         methods: {
@@ -112,8 +94,8 @@
         },
         mounted() {
             this.newElementLabel = JSON.parse(JSON.stringify(this.gridTo.label));
-            dataService.getGrid(this.gridFromProp.id).then(gridFrom => {
-                this.gridFrom = JSON.parse(JSON.stringify(gridFrom));
+            dataService.getGrid(this.gridFrom.id).then(gridFrom => {
+                this.$store.commit('setGridFrom', JSON.parse(JSON.stringify(gridFrom)));
             });
         }
     }
