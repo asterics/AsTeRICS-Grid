09dec1012e6d931a0af143270228767db13ca84f
Alija Sabic <sabic.alija@gmail.com>
2024-11-20 00:11:52 +0100

    Update store: add gridFrom and gridTo

--- src/js/vue/plugins/store.js
+++ src/js/vue/plugins/store.js
@@ -5,13 +5,15 @@ Vue.use(Vuex);
 
 const store = new Vuex.Store({
     state: {
-        // NOTE: dict, dicts, gridData, metadata, gridInstance are objects, thus properties itself are not reactive.
+        // NOTE: Properties of object itself are not reactive.
         //       Always replace the object with a new one to trigger reactivity.
         dict: null,
         dicts: null,
         gridData: null,
         metadata: null,
         gridInstance: null,
+        gridFrom: null,
+        gridTo: null,
         editElementId: null
     },
     mutations: {
@@ -29,6 +31,12 @@ const store = new Vuex.Store({
         },
         setGridInstance(state, payload) {
             state.gridInstance = payload;
+        },
+        setGridFrom(state, payload) {
+            state.gridFrom = payload;
+        },
+        setGridTo(state, payload) {
+            state.gridTo = payload;
         },
         setEditElementId(state, payload) {
             state.editElementId = payload;
