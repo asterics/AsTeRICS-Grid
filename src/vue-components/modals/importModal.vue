3b946b874eada88f8cda041ab66f45f58c3271d2
Alija Sabic <sabic.alija@gmail.com>
2024-11-23 01:28:47 +0100

    Use new modal (importModal)

--- src/vue-components/modals/importModal.vue
+++ src/vue-components/modals/importModal.vue
@@ -1,89 +1,73 @@
 <template>
-    <div class="modal">
-        <div class="modal-mask">
-            <div class="modal-wrapper">
-                <div class="modal-container modal-container-flex" @keydown.esc="$emit('close')" @keydown.ctrl.enter="save()">
-                    <div class="container-fluid px-0 mb-5">
-                        <div class="row">
-                            <div class="modal-header col-8 col-sm-10 col-md-10">
-                                <h1 class="inline">{{ $t('importDataFromFile') }}</h1>
-                            </div>
-                            <a class="col-2 col-sm-1 col-md black" href="javascript:;" @click="openHelp()"><i class="fas fa-question-circle"></i></a>
-                            <a class="col-2 col-sm-1 col-md black" href="javascript:void(0);" :title="$t('close')" @click="$emit('close')"><i class="fas fa-times"/></a>
-                        </div>
-                    </div>
-
-                    <div class="modal-body mt-2">
-                        <div class="row">
-                            <div class="col-12 col-md-6 mb-2">
-                                <label class="me-3" for="fileInput">{{ $t('selectFile') }}</label>
-                                <input id="fileInput" type="file" @change="fileChanged" accept=".grd, .txt"/>
-                            </div>
-                            <div class="col-12 col-md-6" v-if="file && !importData">{{ $t('backupFileDoesntContainData') }}</div>
-                            <div class="col-12 col-md-6" v-if="importData">
-                                <ul>
-                                    <li v-if="hasGrids">
-                                        <span :title="importData.grids.map(g => i18nService.getTranslation(g.label))">{{importData.grids.length}} grid(s)</span>
-                                    </li>
-                                    <li v-if="hasDictionaries">
-                                        <span :title="importData.dictionaries.map(d => d.dictionaryKey)">{{ $t('numDictionaries', {length: importData.dictionaries.length}) }}</span>
-                                    </li>
-                                    <li v-if="hasUserSettings">{{ $t('userSettings') }}</li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="row" v-if="hasDictionaries">
-                            <div>
-                                <input id="importDictionaries" type="checkbox" v-model="options.importDictionaries"/>
-                                <label for="importDictionaries">{{ $t('importDictionaries') }}</label>
-                            </div>
-                        </div>
-                        <div class="row" v-if="hasUserSettings">
-                            <div>
-                                <input id="importUserSettings" type="checkbox" v-model="options.importUserSettings"/>
-                                <label for="importUserSettings">{{ $t('importUserSettings') }}</label>
-                            </div>
-                        </div>
-                        <div class="row" v-if="importData && !hasGlobalGrid">
-                            <div>
-                                <input id="generateGlobalGrid" type="checkbox" v-model="options.generateGlobalGrid"/>
-                                <label for="generateGlobalGrid">{{ $t('generateGlobalGrid') }}</label>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div>
-                                <input id="resetBeforeImport" type="checkbox" v-model="options.resetBeforeImport"/>
-                                <label for="resetBeforeImport">{{ $t('deleteExistingDataBeforeImporting') }}</label>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="modal-footer modal-footer-flex">
-                        <div class="button-container srow">
-                            <button class="six columns" @click="$emit('close')" :title="$t('keyboardEsc')">
-                                <i class="fas fa-times"/> <span>{{ $t('cancel') }}</span>
-                            </button>
-                            <button class="six columns" @click="save()" :disabled="!this.importData" :title="$t('keyboardCtrlEnter')">
-                                <i class="fas fa-check"/> <span>{{ $t('importData') }}</span>
-                            </button>
-                        </div>
-                    </div>
+    <modal :title="$t('importDataFromFile')" :help-fn="openHelp">
+        <template>
+            <div class="row">
+                <div class="col-12 col-md-6 mb-2">
+                    <label class="me-3" for="fileInput">{{ $t('selectFile') }}</label>
+                    <input id="fileInput" type="file" @change="fileChanged" accept=".grd, .txt"/>
+                </div>
+                <div class="col-12 col-md-6" v-if="file && !importData">{{ $t('backupFileDoesntContainData') }}</div>
+                <div class="col-12 col-md-6" v-if="importData">
+                    <ul>
+                        <li v-if="hasGrids">
+                            <span :title="importData.grids.map(g => i18nService.getTranslation(g.label))">{{importData.grids.length}} grid(s)</span>
+                        </li>
+                        <li v-if="hasDictionaries">
+                            <span :title="importData.dictionaries.map(d => d.dictionaryKey)">{{ $t('numDictionaries', {length: importData.dictionaries.length}) }}</span>
+                        </li>
+                        <li v-if="hasUserSettings">{{ $t('userSettings') }}</li>
+                    </ul>
                 </div>
             </div>
-        </div>
-    </div>
+            <div class="row" v-if="hasDictionaries">
+                <div>
+                    <input id="importDictionaries" type="checkbox" v-model="options.importDictionaries"/>
+                    <label for="importDictionaries">{{ $t('importDictionaries') }}</label>
+                </div>
+            </div>
+            <div class="row" v-if="hasUserSettings">
+                <div>
+                    <input id="importUserSettings" type="checkbox" v-model="options.importUserSettings"/>
+                    <label for="importUserSettings">{{ $t('importUserSettings') }}</label>
+                </div>
+            </div>
+            <div class="row" v-if="importData && !hasGlobalGrid">
+                <div>
+                    <input id="generateGlobalGrid" type="checkbox" v-model="options.generateGlobalGrid"/>
+                    <label for="generateGlobalGrid">{{ $t('generateGlobalGrid') }}</label>
+                </div>
+            </div>
+            <div class="row">
+                <div>
+                    <input id="resetBeforeImport" type="checkbox" v-model="options.resetBeforeImport"/>
+                    <label for="resetBeforeImport">{{ $t('deleteExistingDataBeforeImporting') }}</label>
+                </div>
+            </div>
+        </template>
+        <template #ok-button>
+            <button
+                @click="save"
+                @keydown.ctrl.enter="save"
+                :aria-label="$t('importData')"
+                :title="$t('keyboardCtrlEnter')"
+                :disabled="!importData"
+                >
+                <i class="fas fa-check" aria-hidden="true"></i>
+                {{ $t('importData') }}
+            </button>
+        </template>
+    </modal>
 </template>
 
 <script>
     import {dataService} from '../../js/service/data/dataService'
-    import './../../css/modal.css';
+    import { modalMixin } from '../mixins/modalMixin.js';
     import {helpService} from "../../js/service/helpService";
     import {i18nService} from "../../js/service/i18nService.js";
     import {MainVue} from "../../js/vue/mainVue.js";
 
-
     export default {
-        props: ['gridsData', 'reloadFn'],
+        mixins: [modalMixin],
         data: function () {
             return {
                 data: null,
@@ -124,7 +108,6 @@
                 if (!this.importData || (this.options.resetBeforeImport && !confirm(i18nService.t("doYouWantToDeleteBeforeImporting")))) {
                     return;
                 }
-                this.$emit('close');
                 if (this.options.resetBeforeImport) {
                     MainVue.showProgressBar(0, {
                         header: i18nService.t('importDataFromFile'),
@@ -152,16 +135,12 @@
                 if (this.options.resetBeforeImport) {
                     await dataService.markCurrentConfigAsBackedUp();
                 }
-                MainVue.showProgressBar(100);
-                if (this.reloadFn) {
-                    this.reloadFn();
-                }
+                this.$emit('reload');
+                this.$emit('close');
             },
             openHelp() {
                 helpService.openHelp();
             }
-        },
-        mounted() {
         },
         beforeDestroy() {
             helpService.revertToLastLocation();
