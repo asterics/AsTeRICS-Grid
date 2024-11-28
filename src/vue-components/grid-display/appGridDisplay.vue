d76a219cceb32181f199b3d9ddce8500b4442775
Benjamin Klaus <benjamin@asterics-foundation.org>
2024-11-28 12:28:59 +0100

    #451 don't show hidden elements

--- src/vue-components/grid-display/appGridDisplay.vue
+++ src/vue-components/grid-display/appGridDisplay.vue
@@ -1,7 +1,7 @@
 <template>
     <grid-layout v-if="gridData" :rows="gridData.rowCount" :columns="columns" :options="{backgroundColor: metadata.colorConfig.gridBackgroundColor, componentType: 'ol'}">
         <grid-element v-for="elem in gridData.gridElements" :key="elem.id" :x="elem.x" :y="elem.y" :width="elem.width" :height="elem.height" component-type="li">
-            <app-grid-element :grid-element="elem" :metadata="metadata"/>
+            <app-grid-element v-if="!elem.hidden" :grid-element="elem" :metadata="metadata"/>
         </grid-element>
     </grid-layout>
 </template>
