2e30e9ad8748dacf836f63af117a49825d7a3724
Benjamin Klaus <benjamin@asterics-foundation.org>
2024-11-28 11:29:25 +0100

    #451 simplifications in appGridDisplay.vue

--- src/vue-components/grid-display/appGridDisplay.vue
+++ src/vue-components/grid-display/appGridDisplay.vue
@@ -1,6 +1,6 @@
 <template>
-    <grid-layout v-if="renderGrid" :rows="renderGrid.rowCount" :columns="columns" :options="{backgroundColor: metadata.colorConfig.gridBackgroundColor, componentType: 'ol'}">
-        <grid-element v-for="elem in renderGrid.gridElements" :key="elem.id" :x="elem.x" :y="elem.y" :width="elem.width" :height="elem.height" component-type="li">
+    <grid-layout v-if="gridData" :rows="gridData.rowCount" :columns="columns" :options="{backgroundColor: metadata.colorConfig.gridBackgroundColor, componentType: 'ol'}">
+        <grid-element v-for="elem in gridData.gridElements" :key="elem.id" :x="elem.x" :y="elem.y" :width="elem.width" :height="elem.height" component-type="li">
             <app-grid-element :grid-element="elem" :metadata="metadata"/>
         </grid-element>
     </grid-layout>
@@ -9,34 +9,25 @@
 <script>
 
 import GridLayout from '../grid-layout/grid-layout.vue';
-import { gridUtil } from '../../js/util/gridUtil';
 import GridElement from '../grid-layout/grid-element.vue';
 import AppGridElement from './appGridElement.vue';
+import { gridUtil } from '../../js/util/gridUtil';
 
 export default {
     components: { GridElement, GridLayout, AppGridElement },
     props: ["gridData", "metadata"],
     data() {
         return {
-            renderGrid: null,
-            columns: null
-        }
-    },
-    watch: {
-        gridData: function() {
-            this.load();
         }
     },
     computed: {
-    },
-    methods: {
-        load() {
-            this.columns = gridUtil.getWidthWithBounds(this.gridData);
-            this.renderGrid = this.gridData;
+        columns() {
+            return gridUtil.getWidthWithBounds(this.gridData);
         }
     },
+    methods: {
+    },
     mounted() {
-        this.load();
     },
 }
 </script>
