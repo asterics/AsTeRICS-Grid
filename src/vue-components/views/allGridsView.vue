54bb5a98fe490e9f6d8abe50461745255b165dda
Alija Sabic <sabic.alija@gmail.com>
2024-11-28 16:24:43 +0100

    Update gridLinkModal: use base-modal

--- src/vue-components/views/allGridsView.vue
+++ src/vue-components/views/allGridsView.vue
@@ -110,7 +110,7 @@
         </div>
 
         <no-grids-page v-if="graphList && graphList.length === 0 && !showLoading" :restore-backup-handler="importBackup" :import-custom-handler="() => importModal.show = true" :reset-global-grid="this.resetGlobalGrid"></no-grids-page>
-        <grid-link-modal v-if="linkModal.show" :grid-from-prop="linkModal.gridFrom" :grid-to-prop="linkModal.gridTo" @close="linkModal.show = false" @reload="reload(linkModal.gridFrom.id)"></grid-link-modal>
+        <grid-link-modal ref="link" :grid-from-prop="linkModal.gridFrom" :grid-to-prop="linkModal.gridTo" @reload="reload(linkModal.gridFrom.id)"></grid-link-modal>
         <export-pdf-modal v-if="pdfModal.show" :grids-data="grids" :print-grid-id="pdfModal.printGridId" @close="pdfModal.show = false; pdfModal.printGridId = null;"></export-pdf-modal>
         <export-modal v-if="backupModal.show" :grids-data="grids" :export-options="backupModal.exportOptions" @close="backupModal.show = false"></export-modal>
         <import-modal v-if="importModal.show" @close="importModal.show = false" :reload-fn="reload"></import-modal>
@@ -172,7 +172,6 @@
                 selectValue: null,
                 orderValue: localStorageService.get(ORDER_MODE_KEY) || ORDER_VALUES.CONNECTION_COUNT,
                 linkModal: {
-                    show: false,
                     gridFrom: null,
                     gridTo: null
                 },
@@ -588,7 +587,7 @@
                 case "CONTEXT_CONNECT":
                     vueApp.linkModal.gridFrom = vueApp.selectedGraphElement.grid;
                     vueApp.linkModal.gridTo = vueApp.grids.filter(g => g.id === gridId)[0];
-                    vueApp.linkModal.show = true;
+                    vueApp.$refs.link.openModal();
                     break;
                 case "CONTEXT_SHOW":
                     vueApp.show(gridId);
