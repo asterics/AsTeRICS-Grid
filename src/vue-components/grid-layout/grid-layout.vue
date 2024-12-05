a9813cb24e1e58bf029409fa6493202356282e4d
Benjamin Klaus <benjamin@asterics-foundation.org>
2024-12-05 11:05:50 +0100

    #451 added types and default values to props

--- src/vue-components/grid-layout/grid-layout.vue
+++ src/vue-components/grid-layout/grid-layout.vue
@@ -1,5 +1,5 @@
 <template>
-    <component :is="useOptions.componentType" class="grid-layout" :style="`grid-template-columns: repeat(${columns}, minmax(0, 1fr)); grid-template-rows: repeat(${rows}, minmax(0, 1fr)); background-color: ${useOptions.backgroundColor}`">
+    <component :is="options.componentType" class="grid-layout" :style="`grid-template-columns: repeat(${columns}, minmax(0, 1fr)); grid-template-rows: repeat(${rows}, minmax(0, 1fr)); background-color: ${options.backgroundColor}`">
         <slot></slot>
     </component>
 </template>
@@ -7,18 +7,21 @@
 <script>
 
 export default {
-    props: ["rows", "columns", "options"],
-    data() {
-        return {
-            defaultOptions: {
-                backgroundColor: 'white',
-                componentType: 'div'
+    props: {
+        rows: Number,
+        columns: Number,
+        options: {
+            type: Object,
+            default: () => {
+                return {
+                    backgroundColor: 'white',
+                    componentType: 'div'
+                }
             }
         }
     },
-    computed: {
-        useOptions() {
-            return Object.assign(this.defaultOptions, this.options);
+    data() {
+        return {
         }
     },
     methods: {
