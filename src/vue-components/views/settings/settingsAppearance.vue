3ae345891685992498b085f15a1a4fdbc8543696
Benjamin Klaus <benjamin@asterics-foundation.org>
2025-01-08 14:57:24 +0100

    #451 #452 cleanup settingsAppearance.vue

--- src/vue-components/views/settings/settingsAppearance.vue
+++ src/vue-components/views/settings/settingsAppearance.vue
@@ -7,21 +7,21 @@
                     <label class="three columns" for="elemColor">
                         <span>{{ $t('defaultGridElementColor') }}</span>
                     </label>
-                    <input id="elemColor" v-model="metadata.colorConfig.elementBackgroundColor" class="five columns" type="color" @change="saveMetadata()">
-                    <button class="three columns" @click="metadata.colorConfig.elementBackgroundColor = constants.DEFAULT_ELEMENT_BACKGROUND_COLOR; saveMetadata()">{{ $t('reset') }}</button>
+                    <input id="elemColor" v-model="metadata.colorConfig.elementBackgroundColor" class="five columns" type="color" @change="saveMetadata(metadata)">
+                    <button class="three columns" @click="metadata.colorConfig.elementBackgroundColor = constants.DEFAULT_ELEMENT_BACKGROUND_COLOR; saveMetadata(metadata)">{{ $t('reset') }}</button>
                 </div>
                 <div class="srow">
                     <label class="three columns" for="appColor">
                         <span>{{ $t('defaultGridBackgroundColor') }}</span>
                     </label>
-                    <input id="appColor" v-model="metadata.colorConfig.gridBackgroundColor" class="five columns" type="color" @change="saveMetadata()">
-                    <button class="three columns" @click="metadata.colorConfig.gridBackgroundColor = constants.DEFAULT_GRID_BACKGROUND_COLOR; saveMetadata()">{{ $t('reset') }}</button>
+                    <input id="appColor" v-model="metadata.colorConfig.gridBackgroundColor" class="five columns" type="color" @change="saveMetadata(metadata)">
+                    <button class="three columns" @click="metadata.colorConfig.gridBackgroundColor = constants.DEFAULT_GRID_BACKGROUND_COLOR; saveMetadata(metadata)">{{ $t('reset') }}</button>
                 </div>
                 <div class="srow">
                     <label class="three columns" for="colorScheme">
                         <span>{{ $t('colorSchemeForCategories') }}</span>
                     </label>
-                    <select id="colorScheme" class="five columns" v-model="metadata.colorConfig.activeColorScheme" @change="saveMetadata()">
+                    <select id="colorScheme" class="five columns" v-model="metadata.colorConfig.activeColorScheme" @change="saveMetadata(metadata)">
                         <option v-for="scheme in constants.DEFAULT_COLOR_SCHEMES" :value="scheme.name">{{scheme.name | translate}}</option>
                     </select>
                 </div>
@@ -31,7 +31,7 @@
                     </div>
                 </div>
                 <div class="srow">
-                    <input id="colorSchemeActive" type="checkbox" v-model="metadata.colorConfig.colorSchemesActivated" @change="saveMetadata()"/>
+                    <input id="colorSchemeActive" type="checkbox" v-model="metadata.colorConfig.colorSchemesActivated" @change="saveMetadata(metadata)"/>
                     <label for="colorSchemeActive">
                         <span>{{ $t('activateColorCategoriesOfGridElements') }}</span>
                     </label>
@@ -43,7 +43,7 @@
                 <h3 class="mt-2">{{ $t('elementLabels') }}</h3>
                 <div class="srow">
                     <label class="three columns" for="convertText">{{ $t('convertElementLabels') }}</label>
-                    <select id="convertText" v-model="metadata.textConfig.convertMode" class="five columns" @change="saveMetadata()">
+                    <select id="convertText" v-model="metadata.textConfig.convertMode" class="five columns" @change="saveMetadata(metadata)">
                         <option :value="null">{{ $t('dontConvertLabels') }}</option>
                         <option :value="TextConfig.CONVERT_MODE_UPPERCASE">{{ $t('convertToUppercasse') }}</option>
                         <option :value="TextConfig.CONVERT_MODE_LOWERCASE">{{ $t('convertToLowercase') }}</option>
@@ -55,19 +55,17 @@
 </template>
 
 <script>
-    import { i18nService } from '../../../js/service/i18nService';
-    import { util } from '../../../js/util/util';
     import { TextConfig } from '../../../js/model/TextConfig';
     import { MetaData } from '../../../js/model/MetaData';
     import { constants } from '../../../js/util/constants';
+    import { settingsSaveMixin } from './settingsSaveMixin';
 
     export default {
         components: {},
         props: ["metadata", "userSettingsLocal"],
+        mixins: [settingsSaveMixin],
         data() {
             return {
-                i18nService: i18nService,
-                util: util,
                 TextConfig: TextConfig,
                 MetaData: MetaData,
                 constants: constants
