cd73bf6125a48fdd291e05ce4345bfd2aa233735
Benjamin Klaus <benjamin@asterics-foundation.org>
2025-01-08 11:59:13 +0100

    #451 cleanup of settingsGeneral.vue

--- src/vue-components/views/settings/settingsGeneral.vue
+++ src/vue-components/views/settings/settingsGeneral.vue
@@ -36,7 +36,7 @@
             <div class="eleven columns">
                 <h3 class="mt-2">{{ $t('notifications') }}</h3>
                 <div>
-                    <slider-input :label="'intervalForRemindingMakeBackups'" unit="days" id="backupReminderInterval" min="0" max="100" step="1" v-model.number="metadata.notificationConfig.backupNotifyIntervalDays" @change="saveMetadata()"/>
+                    <slider-input :label="'intervalForRemindingMakeBackups'" unit="days" id="backupReminderInterval" min="0" max="100" step="1" v-model.number="metadata.notificationConfig.backupNotifyIntervalDays" @change="saveMetadata(metadata)"/>
                 </div>
             </div>
         </div>
@@ -54,15 +54,7 @@
 <script>
     import {i18nService} from "../../../js/service/i18nService";
     import HeaderIcon from '../../../vue-components/components/headerIcon.vue'
-    import {speechService} from "../../../js/service/speechService";
-    import {util} from "../../../js/util/util";
-    import {localStorageService} from "../../../js/service/data/localStorageService";
-    import {constants} from "../../../js/util/constants.js";
-    import {MetaData} from "../../../js/model/MetaData.js";
-    import {TextConfig} from "../../../js/model/TextConfig.js";
     import Accordion from "../../components/accordion.vue";
-    import {arasaacService} from "../../../js/service/pictograms/arasaacService.js";
-    import {speechServiceExternal} from "../../../js/service/speechServiceExternal.js";
     import SliderInput from '../../modals/input/sliderInput.vue';
     import { settingsSaveMixin } from './settingsSaveMixin';
 
@@ -72,72 +64,11 @@
         mixins: [settingsSaveMixin],
         data() {
             return {
-                show: false,
                 appLanguages: i18nService.getAppLanguages(),
-                allLanguages: i18nService.getAllLanguages(),
-                currentLang: i18nService.getAppLang(),
-                saveSuccess: null,
-                speechService: speechService,
-                voices: [],
-                selectVoices: [],
-                validSpeechServiceUrl: null,
-                externalVoiceCacheProgress: undefined,
-                i18nService: i18nService,
-                localStorageService: localStorageService,
-                constants: constants,
-                MetaData: MetaData,
-                TextConfig: TextConfig,
-                arasaacService: arasaacService,
-                util: util
+                allLanguages: i18nService.getAllLanguages()
             }
         },
         methods: {
-            fixCurrentVoice(dontSave) {
-                if (!this.selectVoices.map(v => v.id).includes(this.userSettingsLocal.voiceConfig.preferredVoice)) {
-                    this.userSettingsLocal.voiceConfig.preferredVoice = undefined;
-                    if (!dontSave) {
-                        this.saveUserSettingsLocal();
-                    }
-                }
-            },
-            getSelectVoices() {
-                if (!this.voices) {
-                    return []
-                }
-                this.sortVoices();
-                if (this.selectAllVoices) {
-                    return this.voices;
-                }
-                return this.voices.filter(v => i18nService.getBaseLang(v.lang) === i18nService.getContentLangBase());
-            },
-            sortVoices() {
-                this.voices.sort(speechService.voiceSortFn);
-            },
-            async reloadVoices() {
-                this.validSpeechServiceUrl = undefined;
-                this.validSpeechServiceUrl = await speechServiceExternal.validateUrl(this.appSettings.externalSpeechServiceUrl);
-                let timeout = this.validSpeechServiceUrl ? 0 : 3000;
-                util.debounce(async () => {
-                    await speechService.reinit();
-                    this.voices = speechService.getVoices();
-                    //this.selectVoices = this.getSelectVoices();
-                }, timeout, 'RELOAD_VOICES');
-            },
-            async saveUserSettingsLocal(dontSaveSettings) {
-                await i18nService.setContentLanguage(this.userSettingsLocal.contentLang, true);
-                //this.selectVoices = this.getSelectVoices();
-                this.fixCurrentVoice(true);
-                this.setVoiceTestText();
-                if (!dontSaveSettings) {
-                    localStorageService.saveUserSettings(this.userSettingsLocal);
-                    this.saveSuccess = true;
-                }
-            },
-            setVoiceTestText() {
-                let voice = this.voices.filter(voice => voice.id === this.userSettingsLocal.voiceConfig.preferredVoice)[0];
-                let voiceLang = voice ? voice.lang : i18nService.getContentLang();
-                //this.testText = i18nService.tl('thisIsAnEnglishSentence', [], i18nService.getBaseLang(voiceLang))
-            }
         },
         async mounted() {
         }
