44a86e0420e1ab494134260167c7e960bf269908
Benjamin Klaus <benjamin@asterics-foundation.org>
2025-01-08 13:13:21 +0100

    #451 cleanup of settingsIntegrations.vue, correctly re-init speech service after saving externalSpeechUrl

--- src/vue-components/views/settings/settingsIntegrations.vue
+++ src/vue-components/views/settings/settingsIntegrations.vue
@@ -27,10 +27,10 @@
         <h3>{{ $t('externalSpeechService') }}</h3>
         <div class="srow">
             <label class="three columns" for="externalSpeechUrl">{{ $t('externalSpeechUrl') }}</label>
-            <input type="text" id="externalSpeechUrl" class="seven columns" v-model="appSettings.externalSpeechServiceUrl" @input="saveAppSettings(reloadVoices)" placeholder="http://localhost:5555"/>
-            <span class="spaced" v-show="validSpeechServiceUrl === undefined"><i class="fas fa-spinner fa-spin"/></span>
-            <span class="spaced" v-show="validSpeechServiceUrl" style="color: green" :title="$t('urlIsValid')"><i class="fas fa-check"/></span>
-            <span class="spaced" v-show="validSpeechServiceUrl === false" style="color: red" :title="$t('urlIsInvalid')"><i class="fas fa-times"/></span>
+            <input type="text" id="externalSpeechUrl" class="seven columns" v-model="appSettings.externalSpeechServiceUrl" @input="onSpeechUrlInput" placeholder="http://localhost:5555"/>
+            <span class="spaced" v-show="urlValid === undefined"><i class="fas fa-spinner fa-spin"/></span>
+            <span class="spaced" v-show="urlValid" style="color: green" :title="$t('urlIsValid')"><i class="fas fa-check"/></span>
+            <span class="spaced" v-show="urlValid === false" style="color: red" :title="$t('urlIsInvalid')"><i class="fas fa-times"/></span>
         </div>
         <div class="mt-3">
             <span class="fa fa-info-circle"></span>
@@ -48,19 +48,35 @@
     import { i18nService } from '../../../js/service/i18nService';
     import { util } from '../../../js/util/util';
     import { arasaacService } from '../../../js/service/pictograms/arasaacService';
+    import { speechServiceExternal } from '../../../js/service/speechServiceExternal';
+    import { speechService } from '../../../js/service/speechService';
+    import { settingsSaveMixin } from './settingsSaveMixin';
 
     export default {
         components: {},
         props: ["metadata", "userSettingsLocal", "appSettings"],
+        mixins: [settingsSaveMixin],
         data() {
             return {
                 i18nService: i18nService,
                 util: util,
                 arasaacService: arasaacService,
-                validSpeechServiceUrl: null
+                urlValid: null
             }
         },
         methods: {
+            async onSpeechUrlInput() {
+                let savedSomething = await this.saveAppSettings(this.appSettings);
+                if (savedSomething) {
+                    this.urlValid = undefined;
+                    this.urlValid = await speechServiceExternal.validateUrl(this.appSettings.externalSpeechServiceUrl);
+                    this.urlValid = this.appSettings.externalSpeechServiceUrl ? this.urlValid : null;
+                    let timeout = this.urlValid ? 0 : 3000;
+                    util.debounce(async () => {
+                        await speechService.reinit();
+                    }, timeout, 'REINIT_SPEECH');
+                }
+            }
         },
         async mounted() {
         }
