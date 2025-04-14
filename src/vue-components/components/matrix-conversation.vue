536d0d81742748b6f6ec63035f884a7022e24896
Benjamin Klaus <benjamin@asterics-foundation.org>
2025-04-14 14:18:28 +0200

    #294 added error handling / infos to matrix conversation

--- src/vue-components/components/matrix-conversation.vue
+++ src/vue-components/components/matrix-conversation.vue
@@ -1,6 +1,25 @@
 <template>
     <div class="row" style="max-height: 100%; overflow-y: scroll" ref="messenger">
-        <div class="col">
+        <div v-if="loading">
+            <span class="col">{{ $t('loading') }}...</span>
+            <i class="fas fa-spinner fa-spin"></i>
+        </div>
+        <div v-if="!matrixRoom && !loading">
+            <i class="fas fa-exclamation-triangle"></i>
+            <span>{{ $t('matrixMessengerNotLoggedInOrNoRoom') }}</span>
+            <div class="mt-5">{{ $t('goToMatrixSettingsToConfigure') }}</div>
+        </div>
+        <div class="col" v-if="matrixRoom && !loading">
+            <div class="mb-5">
+                <h2 class="mb-2">{{ matrixRoom.name }}</h2>
+                <matrix-room-description :room="matrixRoom" :full="true"/>
+                <div v-if="matrixRoom.hasEncryptionStateEvent()">
+                    <i class="fas fa-lock" style="color: darkgreen"></i> <span>{{ $t('endToEndEncrypted') }}</span>
+                </div>
+                <div v-if="!matrixRoom.hasEncryptionStateEvent()">
+                    <i class="fas fa-lock-open" style="color: darkred"></i> <span>{{ $t('notEndToEndEncrypted') }}</span>
+                </div>
+            </div>
             <div class="row" v-if="!matrixMessages.length">
                 <span class="col">{{ $t('noMessagesInRoom') }}.</span>
             </div>
@@ -24,19 +43,26 @@
 <script>
 import '../../css/modal.css';
 import { matrixService } from '../../js/service/matrixMessenger/matrixService';
+import $ from '../../js/externals/jquery';
+import { constants } from '../../js/util/constants';
+import MatrixRoomDescription from './matrix-room-description.vue';
 
 export default {
-    components: {},
+    components: { MatrixRoomDescription },
     props: ["matrixRoomProp"],
     data: function () {
         return {
+            matrixRooms: null,
+            matrixRoom: null,
             matrixMessages: [],
             currentUser: undefined,
-            matrixText: ''
+            matrixText: '',
+            loading: true
         }
     },
     watch: {
         matrixRoomProp() {
+            this.matrixRooms = [];
             this.matrixRoom = this.matrixRoomProp;
             this.init();
         }
@@ -59,15 +85,17 @@ export default {
         scrollDown() {
             this.$nextTick(() => {
                 this.$refs.messenger.scrollTop = this.$refs.messenger.scrollHeight;
-            })
+            });
         },
         async init() {
             if (!this.matrixRoom) {
                 return;
             }
+            this.loading = true;
             matrixService.onMessage(this.onMessageHandler);
             this.currentUser = await matrixService.getUsername();
             this.matrixMessages = await matrixService.getMessageEvents(this.matrixRoom.roomId, 0);
+            this.loading = false;
             this.scrollDown();
         },
         scroll(diff) {
@@ -90,8 +118,9 @@ export default {
         }
     },
     async mounted() {
-        let rooms = await matrixService.getRooms();
-        this.matrixRoom = rooms[0];
+        this.loading = true;
+        this.matrixRooms = await matrixService.getRooms() || [];
+        this.matrixRoom = this.matrixRooms[0];
         await this.init();
         this.loading = false;
         $(document).on(constants.EVENT_MATRIX_SCROLL_UP, this.onScrollUpEvent);
