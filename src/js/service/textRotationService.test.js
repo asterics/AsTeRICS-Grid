3a1ab08f667ee5a5cfcbcac5dd3271a6d2e6d450
Benjamin Klaus <benjamin@asterics-foundation.org>
2025-09-18 14:22:08 +0200

    #608 removed test of non-existing file

--- src/js/service/textRotationService.test.js
+++
@@ -1,9 +0,0 @@
-// Simple test to verify basic functionality
-test('textRotationService - basic functionality test', () => {
-    // This is a placeholder test to ensure the module can be loaded
-    // In a real application, more comprehensive testing would be done
-    // through integration tests or by running the application
-    expect(true).toBe(true);
-});
-
-
