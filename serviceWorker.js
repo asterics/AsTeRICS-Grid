8ff8398f9aa4e28fef2d906cc14b6182d89d6507
Benjamin Klaus <benjamin@asterics-foundation.org>
2025-10-24 14:11:17 +0200

    added bundles for release release-2025-10-24-14.10/+0200

--- serviceWorker.js
+++ serviceWorker.js
@@ -40,7 +40,7 @@ self.addEventListener('install', (event) => {
 self.addEventListener('activate', event => {
     clients.claim();
     sendToClients({type: constants.SW_EVENT_ACTIVATED, activated: true});
-    console.log('Service Worker active! Version: https://github.com/asterics/AsTeRICS-Grid/releases/tag/#ASTERICS_GRID_VERSION#');
+    console.log('Service Worker active! Version: https://github.com/asterics/AsTeRICS-Grid/releases/tag/release-2025-10-24-14.10/+0200');
 });
 
 self.addEventListener('message', (event) => {
