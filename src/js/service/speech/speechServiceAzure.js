4d2fdc801b63dcacb8ca8d858f1f38427fa9c59f
Benjamin Klaus <benjamin@asterics-foundation.org>
2025-12-15 15:56:43 +0100

    #617 first use current url param azure keys

--- src/js/service/speech/speechServiceAzure.js
+++ src/js/service/speech/speechServiceAzure.js
@@ -10,8 +10,8 @@ let speechServiceAzure = {};
 const AZURE_KEY = "AZURE_KEY";
 const AZURE_REGION = "AZURE_REGION";
 
-let msSubscriptionKey = localStorageService.get(AZURE_KEY) || window.azureKey;
-let msRegion = localStorageService.get(AZURE_REGION) || window.azureRegion;
+let msSubscriptionKey =  window.azureKey || localStorageService.get(AZURE_KEY);
+let msRegion = window.azureRegion || localStorageService.get(AZURE_REGION);
 
 // TODO: remove
 localStorageService.save(AZURE_KEY, msSubscriptionKey);
