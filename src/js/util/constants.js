30ca0f6eb7fd584d189a995d84a84337126b1d53
Benjamin Klaus <benjamin@asterics-foundation.org>
2026-01-27 05:53:30 +0100

    #715 added way to force SW use via "serviceworker" tag on PR in automatic build workflow

--- src/js/util/constants.js
+++ src/js/util/constants.js
@@ -80,6 +80,7 @@ constants.ENVIRONMENT = '#ASTERICS_GRID_ENV#';
 constants.IS_ENVIRONMENT_DEV = constants.ENVIRONMENT === '#ASTERICS_' + 'GRID_ENV#';
 constants.IS_ENVIRONMENT_BETA = constants.ENVIRONMENT === 'BETA';
 constants.IS_ENVIRONMENT_PROD = !constants.IS_ENVIRONMENT_DEV && !constants.IS_ENVIRONMENT_BETA;
+constants.FORCE_USE_SW = false;
 constants.CURRENT_VERSION = '#ASTERICS_GRID_VERSION#';
 constants.IS_FIREFOX = navigator.userAgent.indexOf('Firefox') !== -1;
 constants.IS_SAFARI = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
