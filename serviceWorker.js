e3f8e62d831ed89e9de7caabff27967fe9f5ff88
Benjamin Klaus <benjamin@asterics-foundation.org>
2026-01-28 12:03:33 +0100

    #715 added waitUntil in SW to ensure clients.claim() and sendToClients() are finished

--- serviceWorker.js
+++ serviceWorker.js
@@ -88,9 +88,15 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('activate', event => {
-    clients.claim();
-    sendToClients({ type: constants.SW_EVENT_ACTIVATED, activated: true });
-    console.log('Service Worker active! Version: https://github.com/asterics/AsTeRICS-Grid/releases/tag/#ASTERICS_GRID_VERSION#');
+    event.waitUntil((async () => {
+        await clients.claim();
+        await sendToClients({
+            type: constants.SW_EVENT_ACTIVATED,
+            activated: true
+        });
+
+        console.log('Service Worker active! Version: https://github.com/asterics/AsTeRICS-Grid/releases/tag/#ASTERICS_GRID_VERSION#');
+    })());
 });
 
 self.addEventListener('message', async (event) => {
