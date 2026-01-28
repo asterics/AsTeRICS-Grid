0de2c5074749d747deebfd2769df732d8bfbe028
Benjamin Klaus <benjamin@asterics-foundation.org>
2026-01-28 16:11:02 +0100

    #715 try to load images in UI in "cors" mode first (attribute crossorigin), fallback to no-cors

--- src/vue-components/grid-display/grid-elements/gridElementImage.vue
+++ src/vue-components/grid-display/grid-elements/gridElementImage.vue
@@ -1,33 +1,35 @@
 <template>
     <div class="img-container" v-if="imageData" :style="`order: ${metadata.textConfig.textPosition === TextConfig.TEXT_POS_BELOW ? 0 : 1}`">
-        <img v-if="useCrossOrigin" :src="imageData" draggable="false" crossorigin="anonymous" alt=""/>
-        <img v-if="!useCrossOrigin" :src="imageData" draggable="false" alt=""/>
+        <img :src="imageData" draggable="false" :crossorigin="corsActive ? 'anonymous' : null" @error="handleImageError" alt=""/>
     </div>
 </template>
 
 <script>
 import GridElementTextContainer from './gridElementTextContainer.vue';
 import { TextConfig } from '../../../js/model/TextConfig';
-import {constants} from '../../../js/util/constants';
 
 export default {
     components: { GridElementTextContainer },
     props: ["gridImage", "metadata"],
     data() {
         return {
-            TextConfig: TextConfig
+            TextConfig: TextConfig,
+            corsActive: true
         }
     },
     computed: {
         imageData() {
             return this.gridImage ? this.gridImage.data || this.gridImage.url : null;
-        },
-        useCrossOrigin() {
-            return this.gridImage.data ||
-                this.gridImage && this.gridImage.url && constants.KNOWN_CORS_IMAGE_APIS.some(apiUrl => this.gridImage.url.startsWith(apiUrl));
         }
     },
     methods: {
+        handleImageError() {
+            // If the image fails to load and we were trying CORS,
+            // it's likely a CORS mismatch. Try again without it.
+            if (this.corsActive) {
+                this.corsActive = false;
+            }
+        }
     },
     mounted() {
     }
