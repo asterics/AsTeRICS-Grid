6d52bdca7454bf16b9abec5b01b2290e629fca32
Benjamin Klaus <benjamin@asterics-foundation.org>
2026-01-30 11:26:32 +0100

    #748 moving to new data model version 7 which uses username as salt for encryption

--- src/js/util/constants.js
+++ src/js/util/constants.js
@@ -1,7 +1,7 @@
 let constants = {};
 
 constants.ELEMENT_EVENT_ID = 'ELEMENT_EVENT_ID';
-constants.MODEL_VERSION = '{"major": 6, "minor": 0, "patch": 0}';
+constants.MODEL_VERSION = '{"major": 7, "minor": 0, "patch": 0}';
 constants.MODEL_VERSION_LOCAL = '{"major": 1, "minor": 0, "patch": 0}';
 
 constants.LOCAL_NOLOGIN_USERNAME = 'default-user';
