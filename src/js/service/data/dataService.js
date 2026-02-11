bcca849951fa3637e45ef8d431f6dae5404cdd3e
Benjamin Klaus <benjamin@asterics-foundation.org>
2026-02-11 15:03:24 +0100

    Revert "#715 correct mapping of ids for "gridsWithValidThumbnail" at importing data"
    This reverts commit 62cd74ca212c9bcd5013780166157868e444885a.

--- src/js/service/data/dataService.js
+++ src/js/service/data/dataService.js
@@ -688,8 +688,6 @@ dataService.importData = async function (data, options) {
         importData.metadata.lastOpenedGridId = regenerateIdsReturn.idMapping[importData.metadata.lastOpenedGridId];
         importData.metadata.globalGridId = regenerateIdsReturn.idMapping[importData.metadata.globalGridId];
         importData.metadata.homeGridId = regenerateIdsReturn.idMapping[importData.metadata.homeGridId] || null;
-        importData.metadata.gridsWithValidThumbnail = importData.metadata.gridsWithValidThumbnail || [];
-        importData.metadata.gridsWithValidThumbnail = importData.metadata.gridsWithValidThumbnail.map(id => regenerateIdsReturn.idMapping[id]);
     }
     importData.grids.forEach((grid) => {
         let label = i18nService.getTranslation(grid.label);
