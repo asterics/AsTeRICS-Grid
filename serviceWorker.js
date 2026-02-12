95ef5cf3641fea0923260b186ad5526e5873ce9e
Benjamin Klaus <benjamin@asterics-foundation.org>
2026-02-12 13:50:55 +0100

    #715 added comment which allows to quickly test other workbox versions

--- serviceWorker.js
+++ serviceWorker.js
@@ -5,6 +5,9 @@ importScripts(
     'app/lib/workbox/workbox-v7.4.0/workbox-precaching.prod.js',
     'app/lib/workbox/workbox-v7.4.0/workbox-cacheable-response.prod.js'
 );
+/*importScripts(
+    'https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js'
+);*/
 importScripts('serviceWorkerCachePaths.js');
 
 let constants = {};
