f724b0c5f716682dbba8eae673b45c742ea32a60
Benjamin Klaus <benjamin@asterics-foundation.org>
2026-02-20 20:10:24 +0100

    #715 removed typo in src attribute

--- src/js/service/collectElementService.js
+++ src/js/service/collectElementService.js
@@ -426,7 +426,7 @@ async function updateCollectElements(isSecondTry) {
                 let marked = markedImageIndex === index;
                 let imgHTML = null;
                 if (image) {
-                    imgHTML = `<img src="${image}1" height="${imgHeight}" style="height: ${imgHeight}px" onerror="handleError()" ${_useCrossorignAttribute ? 'crossorigin="anonymous"' : ''}/>`;
+                    imgHTML = `<img src="${image}" height="${imgHeight}" style="height: ${imgHeight}px" onerror="handleError()" ${_useCrossorignAttribute ? 'crossorigin="anonymous"' : ''}/>`;
                     totalWidth += elemWidth + 2 * imgMargin;
                 } else {
                     let fontSizeFactor = collectElement.textElemSizeFactor || 1.5;
