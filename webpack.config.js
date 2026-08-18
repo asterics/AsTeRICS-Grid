48d639f56a4a372d37def9a15feb2b26dba5c5aa
Benjamin Klaus <benjamin@asterics-foundation.org>
2026-08-18 10:57:00 +0200

    revert changes in webpack.config.js - dbs nodes info will live in constants.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 let path = require('path');
-const webpack = require('webpack');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = env => {
@@ -35,12 +34,7 @@ module.exports = env => {
         mode: mode,
         devtool: mode === 'production' ? false : 'eval-source-map', // Enable source maps in development
         entry: entryScript,
-        plugins: [
-            new VueLoaderPlugin(),
-            new webpack.DefinePlugin({
-                __DBS_NODES__: getDbsNodesDefinition()
-            })
-        ],
+        plugins: [new VueLoaderPlugin()],
         output: {
             path: path.resolve(__dirname, buildDir),
             publicPath: publicPath,
@@ -75,15 +69,3 @@ module.exports = env => {
         }
     };
 };
-
-function getDbsNodesDefinition() {
-    if (process.env.DBS_NODES) {
-        return JSON.stringify(
-            process.env.DBS_NODES.split(',').map(s => s.trim()).filter(Boolean)
-        );
-    }
-    if (process.env.BACKEND_URL) {
-        return JSON.stringify([process.env.BACKEND_URL]);
-    }
-    return 'null';
-}
